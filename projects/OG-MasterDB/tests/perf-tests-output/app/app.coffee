ff57e95dff1d26e0aec3dfea4e6896e3f1ad3865
Daniel Kwiecinski <daniel@opengamma.com>
2012-05-03 17:47:55 +0100

    separate charts per master

--- projects/OG-MasterDB/tests/perf-tests-output/app/app.coffee
+++ projects/OG-MasterDB/tests/perf-tests-output/app/app.coffee
@@ -21,25 +21,41 @@ deps = for dbtype in ['postgres']
               "app/#{dbtype}_#{master}_#{operation}"
 
 require ['app/theme', 'jquery', 'underscore', 'app/charts'], (theme, $, _, charts) ->
-  $ ->
-    palette = generateColors(60)
-    palette.shift() # ommit white
-    palette.shift() # ommit black
+  $ ->    
     
-    chart = charts.time_series_chart('Operations per second', 'chart')
-  
-    for d in flatten(deps)
-      try
-        require [d], (s) ->           
-          color = palette.shift()
-          operation = s.operation
-          dbtype = s.dbtype
-          master = s.master
-          batchSize = s.batchSize
-          chart.addSeries(charts.line_series("#{dbtype}:#{master} #{operation} (#{batchSize})", s.data, color, color), false)   
-          chart.redraw()
-      catch error
-        "just carry on"
+    for dbtype in ['postgres']
+      for master in [
+        'DbConfigMasterBulkTest',
+        'DbExhangeMasterBulkTest',
+        'DbHolidayMasterBulkTest',
+        'DbHTSMasterBulkTest',
+        'DbMarketDataSnapshotMasterBulkTest',
+        'DbPortfolioMasterBulkTest',
+        'DbPositionMasterBulkTest',
+        'DbSecurityMasterWorkerBulkTest']
           
-      
-    chart
+        palette = generateColors(8)
+        palette.shift() # ommit white
+        palette.shift() # ommit black
+        
+        chart_div = $('<div id="'+master+'" style="width: 1200px; height: 800px; margin: 0 auto"></div>')
+        $('body').append(chart_div)
+        chart = charts.time_series_chart('Operations per second on '+master, master)
+                  
+        for operation in ['correct', 'get', 'insert', 'remove', 'search', 'update']
+          benchmark = "app/#{dbtype}_#{master}_#{operation}"
+          ((c, p) ->
+            try                                                                                                                           
+              require [benchmark], (b) ->                                                                                                         
+                color = p.shift()                                                                                                   
+                operation = b.operation                                                                                                   
+                dbtype = b.dbtype                                                                                                         
+                master = b.master                                                                                                         
+                batchSize = b.batchSize                 
+                c.addSeries(charts.line_series("#{dbtype}:#{master} #{operation} (#{batchSize})", b.data, color, color), false)       
+                c.redraw()                                                                                                            
+            catch error                                                                                                                   
+              "just carry on")(chart, palette)                                                                                                                
+            
+          
+         
--- projects/OG-MasterDB/tests/perf-tests-output/app/app.js
+++ projects/OG-MasterDB/tests/perf-tests-output/app/app.js
@@ -42,30 +42,54 @@
 
   require(['app/theme', 'jquery', 'underscore', 'app/charts'], function(theme, $, _, charts) {
     return $(function() {
-      var chart, d, palette, _i, _len, _ref;
-      palette = generateColors(60);
-      palette.shift();
-      palette.shift();
-      chart = charts.time_series_chart('Operations per second', 'chart');
-      _ref = flatten(deps);
+      var benchmark, chart, chart_div, dbtype, master, operation, palette, _i, _len, _ref, _results;
+      _ref = ['postgres'];
+      _results = [];
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        d = _ref[_i];
-        try {
-          require([d], function(s) {
-            var batchSize, color;
-            color = palette.shift();
-            operation = s.operation;
-            dbtype = s.dbtype;
-            master = s.master;
-            batchSize = s.batchSize;
-            chart.addSeries(charts.line_series("" + dbtype + ":" + master + " " + operation + " (" + batchSize + ")", s.data, color, color), false);
-            return chart.redraw();
-          });
-        } catch (error) {
-          "just carry on";
-        }
+        dbtype = _ref[_i];
+        _results.push((function() {
+          var _j, _len2, _ref2, _results2;
+          _ref2 = ['DbConfigMasterBulkTest', 'DbExhangeMasterBulkTest', 'DbHolidayMasterBulkTest', 'DbHTSMasterBulkTest', 'DbMarketDataSnapshotMasterBulkTest', 'DbPortfolioMasterBulkTest', 'DbPositionMasterBulkTest', 'DbSecurityMasterWorkerBulkTest'];
+          _results2 = [];
+          for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+            master = _ref2[_j];
+            palette = generateColors(8);
+            palette.shift();
+            palette.shift();
+            chart_div = $('<div id="' + master + '" style="width: 1200px; height: 800px; margin: 0 auto"></div>');
+            $('body').append(chart_div);
+            chart = charts.time_series_chart('Operations per second on ' + master, master);
+            _results2.push((function() {
+              var _k, _len3, _ref3, _results3;
+              _ref3 = ['correct', 'get', 'insert', 'remove', 'search', 'update'];
+              _results3 = [];
+              for (_k = 0, _len3 = _ref3.length; _k < _len3; _k++) {
+                operation = _ref3[_k];
+                benchmark = "app/" + dbtype + "_" + master + "_" + operation;
+                _results3.push((function(c, p) {
+                  try {
+                    return require([benchmark], function(b) {
+                      var batchSize, color;
+                      color = p.shift();
+                      operation = b.operation;
+                      dbtype = b.dbtype;
+                      master = b.master;
+                      batchSize = b.batchSize;
+                      c.addSeries(charts.line_series("" + dbtype + ":" + master + " " + operation + " (" + batchSize + ")", b.data, color, color), false);
+                      return c.redraw();
+                    });
+                  } catch (error) {
+                    return "just carry on";
+                  }
+                })(chart, palette));
+              }
+              return _results3;
+            })());
+          }
+          return _results2;
+        })());
       }
-      return chart;
+      return _results;
     });
   });
 
