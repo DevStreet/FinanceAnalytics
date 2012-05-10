2986145a68ca22fe5074f6965aa565bbdae516bc
Kevin Vella <kevin@opengamma.com>
2012-05-10 12:50:48 +0100

    Fixed hibernate_sequence table creation script for SQL Server 2008

--- projects/OG-MasterDB/install/db/create/sqlserver2008/cmn_db/V_45__create_common.sql
+++ projects/OG-MasterDB/install/db/create/sqlserver2008/cmn_db/V_45__create_common.sql
@@ -1,6 +1,6 @@
 -- CREATE SEQUENCE hibernate_sequence START WITH 1 INCREMENT BY 1;
 CREATE TABLE hibernate_sequence (
-  SeqID INT identity(1000,1) PRIMARY KEY,
-  SeqVal VARCHAR(1)
+  next_val numeric(19,0) 
+
 )
 
