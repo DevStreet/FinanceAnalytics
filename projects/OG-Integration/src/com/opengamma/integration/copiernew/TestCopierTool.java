8e80062cb5d81e99ff81a92223be4d539ba75010
Kevin Vella <kevin@opengamma.com>
2012-08-30 15:43:33 +0100

    Update temporary testbed

--- projects/OG-Integration/src/com/opengamma/integration/copiernew/TestCopierTool.java
+++ projects/OG-Integration/src/com/opengamma/integration/copiernew/TestCopierTool.java
@@ -6,10 +6,17 @@
 package com.opengamma.integration.copiernew;
 
 import com.opengamma.component.tool.AbstractTool;
+import com.opengamma.id.ExternalId;
+import com.opengamma.id.ExternalIdBundleWithDates;
+import com.opengamma.id.ExternalIdWithDates;
 import com.opengamma.id.ObjectId;
 import com.opengamma.id.UniqueId;
 import com.opengamma.integration.copiernew.exchange.ExchangeMasterReader;
 import com.opengamma.integration.copiernew.exchange.ExchangeMasterWriter;
+import com.opengamma.integration.copiernew.historicaltimeseries.HistoricalTimeSeriesMasterReader;
+import com.opengamma.integration.copiernew.historicaltimeseries.HistoricalTimeSeriesMasterWriter;
+import com.opengamma.integration.copiernew.historicaltimeseriesdatapoint.HistoricalTimeSeriesDataPointMasterReader;
+import com.opengamma.integration.copiernew.historicaltimeseriesdatapoint.HistoricalTimeSeriesDataPointMasterWriter;
 import com.opengamma.integration.copiernew.holiday.HolidayMasterReader;
 import com.opengamma.integration.copiernew.holiday.HolidayMasterWriter;
 import com.opengamma.integration.copiernew.nodepositionsecurity.NodePositionSecurity;
@@ -26,6 +33,10 @@ import com.opengamma.integration.copiernew.security.SecurityMasterReader;
 import com.opengamma.integration.copiernew.security.SecurityMasterWriter;
 import com.opengamma.master.exchange.ExchangeSearchRequest;
 import com.opengamma.master.exchange.ManageableExchange;
+import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesInfoDocument;
+import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesInfoSearchRequest;
+import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesSelector;
+import com.opengamma.master.historicaltimeseries.ManageableHistoricalTimeSeriesInfo;
 import com.opengamma.master.holiday.HolidaySearchRequest;
 import com.opengamma.master.holiday.ManageableHoliday;
 import com.opengamma.master.portfolio.ManageablePortfolio;
@@ -36,6 +47,8 @@ import com.opengamma.master.position.ManageablePosition;
 import com.opengamma.master.position.PositionSearchRequest;
 import com.opengamma.master.security.ManageableSecurity;
 import com.opengamma.master.security.SecuritySearchRequest;
+import com.opengamma.util.timeseries.localdate.LocalDateDoubleTimeSeries;
+import com.opengamma.util.tuple.ObjectsPair;
 
 public class TestCopierTool extends AbstractTool {
 
@@ -52,14 +65,48 @@ public class TestCopierTool extends AbstractTool {
   @Override
   protected void doRun() throws Exception {
 
+    // Multi time-series copier
+//    HistoricalTimeSeriesInfoSearchRequest searchRequest = new HistoricalTimeSeriesInfoSearchRequest();
+//    searchRequest.setName("KV*");
+//    Iterable<ObjectsPair<ManageableHistoricalTimeSeriesInfo, Iterable<LocalDateDoubleTimeSeries>>>
+//        reader = new HistoricalTimeSeriesMasterReader(
+//            getToolContext().getHistoricalTimeSeriesMaster(),
+//            searchRequest
+//        );
+//    Writeable<ObjectsPair<ManageableHistoricalTimeSeriesInfo, Iterable<LocalDateDoubleTimeSeries>>>
+//        writer = new HistoricalTimeSeriesMasterWriter(getToolContext().getHistoricalTimeSeriesMaster(), "KV<name>");
+//    writer.addOrUpdate(reader);
+
+//    // Time-series data point copier
+//    Iterable<LocalDateDoubleTimeSeries> reader = new HistoricalTimeSeriesDataPointMasterReader(
+//        getToolContext().getHistoricalTimeSeriesMaster(), UniqueId.of("DbHts", "1000")
+//    );
+//
+//    ManageableHistoricalTimeSeriesInfo info = new ManageableHistoricalTimeSeriesInfo();
+//    info.setName("KV Test");
+//    info.setDataField("KVDATA");
+//    info.setDataSource("KVSOURCE");
+//    info.setDataProvider("KVPROVIDER");
+//    info.setObservationTime("KVTIME");
+//    info.setExternalIdBundle(ExternalIdBundleWithDates.of(ExternalIdWithDates.parse("KV~666")));
+//    HistoricalTimeSeriesInfoDocument doc = getToolContext().getHistoricalTimeSeriesMaster().add(
+//        new HistoricalTimeSeriesInfoDocument(info)
+//    );
+//    Writeable<LocalDateDoubleTimeSeries> writer = new HistoricalTimeSeriesDataPointMasterWriter(
+//        getToolContext().getHistoricalTimeSeriesMaster(),
+//        //UniqueId.parse("DbHts~11360")
+//        doc.getInfo().getUniqueId()
+//    );
+//    writer.addOrUpdate(reader);
+
     // Holiday copier
-    HolidaySearchRequest searchRequest = new HolidaySearchRequest();
-    searchRequest.setName("ADALV");
-    Iterable<ManageableHoliday> reader =
-        new HolidayMasterReader(getToolContext().getHolidayMaster(), searchRequest);
-    Writeable<ManageableHoliday> writer =
-        new HolidayMasterWriter(getToolContext().getHolidayMaster());
-    writer.addOrUpdate(reader);
+//    HolidaySearchRequest searchRequest = new HolidaySearchRequest();
+//    searchRequest.setName("ADALV");
+//    Iterable<ManageableHoliday> reader =
+//        new HolidayMasterReader(getToolContext().getHolidayMaster(), searchRequest);
+//    Writeable<ManageableHoliday> writer =
+//        new HolidayMasterWriter(getToolContext().getHolidayMaster());
+//    writer.addOrUpdate(reader);
 
     // Portfolio copier (no contents)
 //    PortfolioSearchRequest searchRequest = new PortfolioSearchRequest();
