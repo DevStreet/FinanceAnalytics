5d57294205f83f2db5a0b30582eabb8f74cec224
Stuart Archibald <stuart@opengamma.com>
2012-09-13 14:06:50 +0100

    string buf overflow fix

--- projects/OG-Maths/src/com/opengamma/maths/lowlevelapi/exposedapi/fortran/BLAS_tests/testdspmv.f90
+++ projects/OG-Maths/src/com/opengamma/maths/lowlevelapi/exposedapi/fortran/BLAS_tests/testdspmv.f90
@@ -9,10 +9,9 @@ integer::  n, incx, incy
 parameter(n=5, incx=1,incy=1)
 integer:: j, i, ptr
 double precision:: A(n,n), AP(((n*(n+1))/2)), x(n), y(n), alpha, beta
-character(2):: length1, length2
+character(2):: length
 external dspmv
-write(length1,"(I2)")((n*(n+1))/2)
-write(length2,"(I2)")n
+write(length,"(I2)")((n*(n+1))/2)
 
 !e some data for A
 do j = 1,n
@@ -48,12 +47,12 @@ print*,""
 print*,""
 do i=1,n
         write(*,"(A3)",ADVANCE="NO")"A ="
-        write(*,"("//trim(adjustl(length2))//"(2X,F5.2))"),A(i,:)
+        write(*,"("//trim(adjustl(length))//"(2X,F5.2))"),A(i,:)
 enddo
 print*,""
 print*,""
 write(*,"(A3)",ADVANCE="NO")"AP="
-write(*,"("//trim(adjustl(length1))//"(2X,F5.2))"),AP(:)
+write(*,"("//trim(adjustl(length))//"(2X,F5.2))"),AP(:)
 
 
 alpha=2d0
@@ -64,6 +63,6 @@ call  dspmv("U",n,alpha,AP,x,incx,beta,y,incy)
 print*,""
 print*,"output"
 write(*,"(A2)")"y="
-write(*,"("//trim(adjustl(length2))//"(2X,F8.2))"),y
+write(*,"("//trim(adjustl(length))//"(2X,F8.2))"),y
 
 end program testdspmv
