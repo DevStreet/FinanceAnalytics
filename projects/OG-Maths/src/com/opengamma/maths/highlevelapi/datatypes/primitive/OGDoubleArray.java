51155d894ca15bafb2ddf8aed588b0f93aa2174f
Stuart Archibald <stuart@opengamma.com>
2012-10-11 15:12:06 +0100

    Added unit tests for OGDoubleArray

--- projects/OG-Maths/src/com/opengamma/maths/highlevelapi/datatypes/primitive/OGDoubleArray.java
+++ projects/OG-Maths/src/com/opengamma/maths/highlevelapi/datatypes/primitive/OGDoubleArray.java
@@ -8,7 +8,8 @@ package com.opengamma.maths.highlevelapi.datatypes.primitive;
 import java.util.Arrays;
 
 import com.opengamma.maths.commonapi.exceptions.MathsExceptionIllegalArgument;
-import com.opengamma.maths.commonapi.exceptions.MathsExceptionOutOfBounds;
+import com.opengamma.maths.commonapi.exceptions.MathsExceptionNullPointer;
+import com.opengamma.maths.lowlevelapi.functions.checkers.Catchers;
 import com.opengamma.maths.lowlevelapi.functions.memory.DenseMemoryManipulation;
 
 /**
@@ -26,6 +27,7 @@ public class OGDoubleArray extends OGArraySuper<Number> {
    * @param dataIn a row major java double[][] 
    */
   public OGDoubleArray(double[][] dataIn) {
+    Catchers.catchNullFromArgList(dataIn, 1);
     _data = DenseMemoryManipulation.convertRowMajorDoublePointerToColumnMajorSinglePointer(dataIn);
     _rows = dataIn.length;
     _columns = dataIn[0].length;
@@ -39,7 +41,7 @@ public class OGDoubleArray extends OGArraySuper<Number> {
    */
   public OGDoubleArray(double[] dataIn, int rows, int columns) {
     if (dataIn == null) {
-      throw new MathsExceptionIllegalArgument("dataIn is null");
+      throw new MathsExceptionNullPointer("dataIn is null");
     }
     if (rows < 1) {
       throw new MathsExceptionIllegalArgument("Illegal number of rows specified. Value given was " + rows);
@@ -83,12 +85,12 @@ public class OGDoubleArray extends OGArraySuper<Number> {
       throw new MathsExceptionIllegalArgument("OGDoubleArray only has 2 indicies, more than 2 were given");
     }
     if (indices[0] >= _rows) {
-      throw new MathsExceptionOutOfBounds("Row index" + indices[0] + " requested for matrix with only " + _rows + " rows");
+      throw new MathsExceptionIllegalArgument("Row index" + indices[0] + " requested for matrix with only " + _rows + " rows");
     }
     if (indices[1] >= _columns) {
-      throw new MathsExceptionOutOfBounds("Columns index" + indices[1] + " requested for matrix with only " + _columns + " columns");
+      throw new MathsExceptionIllegalArgument("Columns index" + indices[1] + " requested for matrix with only " + _columns + " columns");
     }
-    return _data[indices[0] * _rows + indices[1]];
+    return _data[indices[1] * _rows + indices[0]];
   }
 
   public OGDoubleArray getFullRow(int index) {
@@ -144,7 +146,13 @@ public class OGDoubleArray extends OGArraySuper<Number> {
     return new OGDoubleArray(rowData, 1, _columns);
   }
 
-  public boolean fuzzyequals(OGDoubleArray obj, double tolerance) {
+  /**
+   * Decide if this {@link OGDoubleArray} is equal to another {@link OGDoubleArray} with the addition of some numerical tolerance for floating point comparison
+   * @param obj the {@link OGDoubleArray} against which a comparison is to be drawn
+   * @param tolerance the tolerance for double precision comparison of the data elements in the array
+   * @return true if they are considered equal in value, false otherwise
+   */
+  public boolean fuzzyequals(Object obj, double tolerance) {
     if (this == obj) {
       return true;
     }
@@ -154,14 +162,14 @@ public class OGDoubleArray extends OGArraySuper<Number> {
     if (getClass() != obj.getClass()) {
       return false;
     }
-    OGDoubleArray other = obj;
+    OGDoubleArray other = (OGDoubleArray) obj;
     if (_columns != other._columns) {
       return false;
     }
     if (_rows != other._rows) {
       return false;
     }
-    double[] objData = obj.getData();
+    double[] objData = other.getData();
     for (int i = 0; i < _data.length; i++) {
       if (Math.abs(_data[i] - objData[i]) > tolerance) {
         return false;
