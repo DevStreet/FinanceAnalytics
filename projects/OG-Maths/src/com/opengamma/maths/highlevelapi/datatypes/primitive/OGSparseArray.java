89b65e8a77a5c89c6176b1b09eaba93ddcd1a5b9
Stuart Archibald <stuart@opengamma.com>
2012-10-18 13:10:25 +0100

    Unit tests and code for CSC

--- projects/OG-Maths/src/com/opengamma/maths/highlevelapi/datatypes/primitive/OGSparseArray.java
+++ projects/OG-Maths/src/com/opengamma/maths/highlevelapi/datatypes/primitive/OGSparseArray.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 
 import com.opengamma.maths.commonapi.exceptions.MathsExceptionIllegalArgument;
 import com.opengamma.maths.lowlevelapi.datatypes.primitive.MatrixPrimitiveUtils;
+import com.opengamma.maths.lowlevelapi.functions.checkers.Catchers;
 
 /**
  * 
@@ -30,7 +31,7 @@ public class OGSparseArray extends OGArraySuper<Number> {
    * @param matrix is a double[][]
    */
   public OGSparseArray(double[][] matrix) {
-
+    Catchers.catchNullFromArgList(matrix, 1);
     if (MatrixPrimitiveUtils.isRagged(matrix)) {
       throw new MathsExceptionIllegalArgument("Matrix representation must not be ragged, i.e. all rows must be the same length");
     }
@@ -100,6 +101,20 @@ public class OGSparseArray extends OGArraySuper<Number> {
    * @param columns the number of columns
    */
   public OGSparseArray(int[] colPtr, int[] rowIdx, double[] values, int rows, int columns) {
+    Catchers.catchNullFromArgList(colPtr, 1);
+    Catchers.catchNullFromArgList(rowIdx, 2);
+    Catchers.catchNullFromArgList(values, 3);
+    if (rows < 1) {
+      throw new MathsExceptionIllegalArgument("Illegal number of rows specified. Value given was " + rows);
+    }
+    if (columns < 1) {
+      throw new MathsExceptionIllegalArgument("Illegal number of columns specified. Value given was " + columns);
+    }
+
+    if (columns != colPtr.length - 1) {
+      throw new MathsExceptionIllegalArgument("Columns specified does not commute with length of colPtr. Length colPtr (-1)= " + (colPtr.length - 1) + ", columns given = " + columns + ".");
+    }
+
     final int vlen = values.length;
     _values = new double[vlen];
     System.arraycopy(values, 0, _values, 0, vlen);
@@ -113,26 +128,32 @@ public class OGSparseArray extends OGArraySuper<Number> {
     System.arraycopy(rowIdx, 0, _rowIdx, 0, rlen);
 
     _rows = rows;
-    //TODO: Check columns == colPtr.length - 1
+
     _cols = columns;
 
     _els = rows * columns;
   }
 
-  public double[] getFullColumn(int index) {
+  public OGDoubleArray getFullColumn(int index) {
+    if (index < 0 || index >= _cols) {
+      throw new MathsExceptionIllegalArgument("Invalid index. Value given was " + index);
+    }
     double[] tmp = new double[_cols];
-    for (int i = _colPtr[index]; i <= _colPtr[index + 1] - 1; i++) { // loops through elements of correct column
+    for (int i = _colPtr[index]; i < _colPtr[index + 1]; i++) { // loops through elements of correct column
       tmp[_rowIdx[i]] = _values[i];
     }
-    return tmp;
+    return new OGDoubleArray(tmp, _rows, 1);
   }
 
-  public double[] getFullRow(int index) { // getting rows in CSC form is generally bad
+  public OGDoubleArray getFullRow(int index) { // getting rows in CSC form is generally bad
+    if (index < 0 || index >= _rows) {
+      throw new MathsExceptionIllegalArgument("Invalid index. Value given was " + index);
+    }
     double[] tmp = new double[_rows];
     for (int i = 0; i < _rows; i++) {
       tmp[i] = this.getEntry(index, i);
     }
-    return tmp;
+    return new OGDoubleArray(tmp, 1, _cols);
   }
 
   public int getNumberOfNonZeroElements() {
@@ -156,14 +177,6 @@ public class OGSparseArray extends OGArraySuper<Number> {
   }
 
   /**
-   * Gets the non-zero values in the matrix, i.e. the values that are worth storing
-   * @return _values, the non-zero values
-   */
-  public double[] getNonZeroElements() {
-    return _values;
-  }
-
-  /**
    * Gets the data backing
    * @return _values, the non-zero values
    */
@@ -183,6 +196,15 @@ public class OGSparseArray extends OGArraySuper<Number> {
 
   @Override
   public Double getEntry(int... indices) {
+    if (indices.length > 2) {
+      throw new MathsExceptionIllegalArgument("OGDoubleArray only has 2 indicies, more than 2 were given");
+    }
+    if (indices[0] >= _rows) {
+      throw new MathsExceptionIllegalArgument("Row index" + indices[0] + " requested for matrix with only " + _rows + " rows");
+    }
+    if (indices[1] >= _cols) {
+      throw new MathsExceptionIllegalArgument("Columns index" + indices[1] + " requested for matrix with only " + _cols + " columns");
+    }
     //translate an index and see if it exists, if it doesn't then return 0
     for (int i = _colPtr[indices[1]]; i <= _colPtr[indices[1] + 1] - 1; i++) { // loops through elements of correct column
       // looks for col index
@@ -197,6 +219,44 @@ public class OGSparseArray extends OGArraySuper<Number> {
   public String toString() {
     return "\nvalues=" + Arrays.toString(_values) + "\nrowInd=" + Arrays.toString(_rowIdx) + "\ncolPtr=" + Arrays.toString(_colPtr) + "\ncols=" + _rows + "\nrows=" + _cols + "\nels=" + _els;
 
+  }
+
+  /**
+   * Decide if this {@link OGSparseArray} is equal to another {@link OGSparseArray} with the addition of some numerical tolerance for floating point comparison
+   * @param obj the {@link OGSparseArray} against which a comparison is to be drawn
+   * @param tolerance the tolerance for double precision comparison of the data elements in the array
+   * @return true if they are considered equal in value, false otherwise
+   */
+  public boolean fuzzyequals(Object obj, double tolerance) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    OGSparseArray other = (OGSparseArray) obj;
+    if (_cols != other.getNumberOfColumns()) {
+      return false;
+    }
+    if (_rows != other.getNumberOfRows()) {
+      return false;
+    }
+    if (!Arrays.equals(_colPtr, other.getColumnPtr())) {
+      return false;
+    }
+    if (!Arrays.equals(_rowIdx, other.getRowIndex())) {
+      return false;
+    }
+    double[] objData = other.getData();
+    for (int i = 0; i < _values.length; i++) {
+      if (Math.abs(_values[i] - objData[i]) > tolerance) {
+        return false;
+      }
+    }
+    return true;
   }
 
   @Override
---
+++ projects/OG-Maths/tests/unit/com/opengamma/maths/highlevelapi/datatypes/primitive/OGSparseArrayTest.java
@@ -0,0 +1,376 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.maths.highlevelapi.datatypes.primitive;
+
+import java.util.Arrays;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
+
+import com.opengamma.maths.commonapi.exceptions.MathsExceptionIllegalArgument;
+import com.opengamma.maths.commonapi.exceptions.MathsExceptionNullPointer;
+import com.opengamma.maths.lowlevelapi.linearalgebra.blas.ogblas.auxiliary.D1MACH;
+
+/**
+ * Tests the OGDoubleArray Class
+ */
+public class OGSparseArrayTest {
+
+  double[][] data = { {1, 2, 0, 0 }, {3, 0, 4, 0 }, {0, 5, 6, 0 }, {0, 0, 7, 0 } };
+  double[] compressedData = {1.0, 3.0, 2.0, 5.0, 4.0, 6.0, 7.0 };
+  int[] compressedColPtr = {0, 2, 4, 7, 7 };
+  int[] compressedRowIdx = {0, 1, 0, 2, 1, 2, 3 };
+
+  OGDoubleArray getRow = new OGDoubleArray(new double[] {0, 5, 6, 0 }, 1, 4);
+  OGDoubleArray getCol = new OGDoubleArray(new double[] {2, 0, 5, 0 }, 4, 1);
+
+  // sending in null ptr double[][] constructor
+  @Test(expectedExceptions = MathsExceptionNullPointer.class)
+  public void testDoublePtrPtrConstructorNullPtrTest() {
+    double[][] tmp = null;
+    new OGSparseArray(tmp);
+  }
+
+  // sending in embedded null ptr double[][] constructor
+  @Test(expectedExceptions = MathsExceptionNullPointer.class)
+  public void testDoublePtrPtrConstructorOKThenNullPtrTest() {
+    double[][] tmp = new double[2][1];
+    tmp[0] = new double[] {1 };
+    tmp[1] = null;
+    new OGSparseArray(tmp);
+  }
+
+  // sending in null ptr pos1 int[], int[], double[], int, int constructor  
+  @Test(expectedExceptions = MathsExceptionNullPointer.class)
+  public void testCSCConstructorNullPtrPos1Test() {
+    new OGSparseArray(null, new int[1], new double[1], 1, 1);
+  }
+
+  // sending in null ptr pos1 int[], int[], double[], int, int constructor  
+  @Test(expectedExceptions = MathsExceptionNullPointer.class)
+  public void testCSCConstructorNullPtrPos2Test() {
+    new OGSparseArray(new int[1], null, new double[1], 1, 1);
+  }
+
+  // sending in null ptr pos1 int[], int[], double[], int, int constructor  
+  @Test(expectedExceptions = MathsExceptionNullPointer.class)
+  public void testCSCConstructorNullPtrPos3Test() {
+    new OGSparseArray(new int[1], new int[1], null, 1, 1);
+  }
+
+  // sending in ragged[][] double[][] constructor
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testDoublePtrPtrConstructorRaggedTest() {
+    double[][] tmp = new double[2][];
+    tmp[0] = new double[] {1 };
+    tmp[1] = new double[] {1, 2, 3 };
+    new OGSparseArray(tmp);
+  }
+
+  //sending in all zeros to double[][] constructor
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testDoublePtrPtrConstructorAllZerosTest() {
+    new OGSparseArray(new double[][] { {0, 0, 0 }, {0, 0, 0 } });
+  }
+
+  // sending in ok double[][] constructor
+  @Test
+  public void testDoublePtrPtrConstructorInternalDataTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertTrue(D.getClass() == OGSparseArray.class);
+    assertTrue(Arrays.equals(D.getData(), compressedData));
+    assertTrue(D.getNumberOfRows() == 4);
+    assertTrue(D.getNumberOfColumns() == 4);
+  }
+
+  //sending in bad rows int[], int[], double[], int, int  constructor
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testDoublePtrConstructorBadRowsDataTest() {
+    new OGSparseArray(compressedColPtr, compressedRowIdx, compressedData, -1, 4);
+  }
+
+  //sending in bad cols int[], int[], double[], int, int  constructor
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testDoublePtrConstructorBadColsDataTest() {
+    new OGSparseArray(compressedColPtr, compressedRowIdx, compressedData, 4, -1);
+  }
+
+  //sending in too many cols int[], int[], double[], int, int  constructor
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testDoublePtrConstructorDataCountDataTest() {
+    new OGSparseArray(compressedColPtr, compressedRowIdx, compressedData, 4, 5);
+  }
+
+  //  // sending in ok  int[], int[], double[], int, int  constructor
+  @Test
+  public void testDoublePtrConstructorInternalDataTest() {
+    OGSparseArray D = new OGSparseArray(compressedColPtr, compressedRowIdx, compressedData, 4, 4);
+    assertTrue(D.getClass() == OGSparseArray.class);
+    assertTrue(Arrays.equals(D.getData(), compressedData));
+    assertTrue(Arrays.equals(D.getColumnPtr(), compressedColPtr));
+    assertTrue(Arrays.equals(D.getRowIndex(), compressedRowIdx));
+    assertTrue(D.getNumberOfRows() == 4);
+    assertTrue(D.getNumberOfColumns() == 4);
+  }
+
+  // test get rows
+  @Test
+  public void testGetRowsTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertTrue(D.getNumberOfRows() == 4);
+  }
+
+  // test get cols
+  @Test
+  public void testGetColumnsTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertTrue(D.getNumberOfColumns() == 4);
+  }
+
+  // test get entry bad index count
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testGetEntryBadIndexCountTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    D.getEntry(1, 2, 3);
+  }
+
+  // test get entry bad row index
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testGetEntryBadRowIndicesTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    D.getEntry(23, 1);
+  }
+
+  // test get entry bad col index
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testGetEntryBadColumnIndicesTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    D.getEntry(1, 23);
+  }
+
+  // test get entry ok
+  @Test
+  public void testGetEntryOKIndicesTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    for (int i = 0; i < D.getNumberOfRows(); i++) {
+      for (int j = 0; j < D.getNumberOfColumns(); j++) {
+        assertTrue(D.getEntry(i, j) == data[i][j]);
+      }
+    }
+  }
+
+  // test get full row neg index
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testGetFullRowNegIndexTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    D.getFullRow(-1);
+  }
+
+  // test get full row bad index
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testGetFullRowBadIndexTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    D.getFullRow(23);
+  }
+
+  // test get full row ok
+  @Test
+  public void testGetFullRowOkIndexTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGDoubleArray row = D.getFullRow(2);
+    assertTrue(row.equals(getRow));
+  }
+
+  // test get full col neg index
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testGetFullColNegIndexTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    D.getFullColumn(-1);
+  }
+
+  // test get full col bad index
+  @Test(expectedExceptions = MathsExceptionIllegalArgument.class)
+  public void testGetFullColumnBadIndexTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    D.getFullColumn(23);
+  }
+
+  // test get full col ok
+  @Test
+  public void testGetFullColumnOkIndexTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGDoubleArray col = D.getFullColumn(1);
+    assertTrue(col.equals(getCol));
+  }
+
+  // test get No elements
+  @Test
+  public void testGetNoElementsTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertTrue(D.getNumberOfNonZeroElements() == 7);
+  }
+
+  // test get data
+  @Test
+  public void testGetDataTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertTrue(Arrays.equals(D.getData(), compressedData));
+  }
+
+  // test equals obj points to obj
+  @Test
+  public void testEqualsObjeqObj() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertTrue(D.equals(D));
+  }
+
+  // test equals obj not = null
+  @Test
+  public void testEqualsObjNull() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertFalse(D.equals(null));
+  }
+
+  // test equals obj class different
+  @Test
+  public void testEqualsObjDifferentClass() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertFalse(D.equals(new Double(1.)));
+  }
+
+  // test equals obj class ok, diff cols
+  @Test
+  public void testEqualsObjDifferentCols() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] {{7 } });
+    assertFalse(D.equals(Diff));
+  }
+
+  // test equals obj class ok, same cols diff rows
+  @Test
+  public void testEqualsObjDifferentRows() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] {{1, 2, 3, 4 } });
+    assertFalse(D.equals(Diff));
+  }
+
+  // test equals obj class ok, same cols same rows different data
+  @Test
+  public void testEqualsObjDifferentData() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] { {999, 2, 0, 0 }, {3, 0, 4, 0 }, {0, 5, 6, 0 }, {0, 0, 7, 0 } });
+    assertFalse(D.equals(Diff));
+  }
+
+  // test equals obj class ok, same cols same rows different col ptr
+  @Test
+  public void testEqualsObjDifferentColPtr() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] { {1, 0, 0, 0 }, {3, 0, 4, 0 }, {0, 5, 6, 0 }, {0, 0, 7, 0 } });
+    assertFalse(D.equals(Diff));
+  }
+
+  // test equals obj class ok, same cols same rows same data, should bork on column ptr
+  @Test
+  public void testEqualsObjDifferentRowIdx() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] { {1, 2, 0, 0 }, {3, 0, 4, 0 }, {5, 0, 6, 0 }, {0, 0, 7, 0 } });
+    assertFalse(D.equals(Diff));
+  }
+
+  // test equals obj class ok, same cols same rows same data
+  @Test
+  public void testEqualsObjStructurallyIdentical() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] { {1, 2, 0, 0 }, {3, 0, 4, 0 }, {0, 5, 6, 0 }, {0, 0, 7, 0 } });
+    assertTrue(D.equals(Diff));
+  }
+
+  // test fuzzy equals obj points to obj
+  @Test
+  public void testFuzzyEqualsObjeqObj() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertTrue(D.fuzzyequals(D, 10 * D1MACH.four()));
+  }
+
+  // test fuzzy equals obj not = null
+  @Test
+  public void testFuzzyEqualsObjNull() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertFalse(D.fuzzyequals(null, 10 * D1MACH.four()));
+  }
+
+  // test fuzzy equals obj class different
+  @Test
+  public void testFuzzyEqualsObjDifferentClass() {
+    OGSparseArray D = new OGSparseArray(data);
+    assertFalse(D.fuzzyequals(new Double(1.), 10 * D1MACH.four()));
+  }
+
+  // test fuzzy equals obj class ok, diff cols
+  @Test
+  public void testFuzzyEqualsObjDifferentCols() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] {{7 } });
+    assertFalse(D.fuzzyequals(Diff, 10 * D1MACH.four()));
+  }
+
+  // test fuzzy equals obj class ok, same cols diff rows
+  @Test
+  public void testFuzzyEqualsObjDifferentRows() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] {{1, 2, 3, 4 } });
+    assertFalse(D.fuzzyequals(Diff, 10 * D1MACH.four()));
+  }
+
+  // test fuzzy equals obj class ok, same cols same rows different data
+  @Test
+  public void testFuzzyEqualsObjDifferentData() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] { {999, 2, 0, 0 }, {3, 0, 4, 0 }, {0, 5, 6, 0 }, {0, 0, 7, 0 } });
+    assertFalse(D.fuzzyequals(Diff, 10 * D1MACH.four()));
+  }
+
+  // test equals obj class ok, same cols same rows different col ptr
+  @Test
+  public void testFuzzyEqualsObjDifferentColPtr() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] { {1, 0, 0, 0 }, {3, 0, 4, 0 }, {0, 5, 6, 0 }, {0, 0, 7, 0 } });
+    assertFalse(D.fuzzyequals(Diff, 10 * D1MACH.four()));
+  }
+
+  // test equals obj class ok, same cols same rows same data, should bork on column ptr
+  @Test
+  public void testFuzzyEqualsObjDifferentRowIdx() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Diff = new OGSparseArray(new double[][] { {1, 2, 0, 0 }, {3, 0, 4, 0 }, {5, 0, 6, 0 }, {0, 0, 7, 0 } });
+    assertFalse(D.fuzzyequals(Diff, 10 * D1MACH.four()));
+  }
+
+  // test fuzzy equals obj class ok, same cols same rows same data
+  @Test
+  public void testFuzzyEqualsObjStructurallyIdentical() {
+    OGSparseArray D = new OGSparseArray(data);
+    OGSparseArray Same = new OGSparseArray(new double[][] { {1 + 9 * D1MACH.four(), 2, 0, 0 }, {3, 0, 4, 0 }, {0, 5, 6, 0 }, {0, 0, 7, 0 } });
+    assertTrue(D.fuzzyequals(Same, 10 * D1MACH.four()));
+  }
+
+  // test hash code
+  @Test
+  public void testHashCodeTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    D.hashCode();
+  }
+
+  // test toString code
+  @Test
+  public void testToStringTest() {
+    OGSparseArray D = new OGSparseArray(data);
+    D.toString();
+  }
+
+}
