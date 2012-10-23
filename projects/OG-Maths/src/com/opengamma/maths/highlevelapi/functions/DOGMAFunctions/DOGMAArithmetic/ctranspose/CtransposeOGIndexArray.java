75a91a45ea249fc2fcf5d2b6befe7705b175af5b
Stuart Archibald <stuart@opengamma.com>
2012-10-23 12:22:45 +0100

    Conjugate transpose on index arrays

---
+++ projects/OG-Maths/src/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMAArithmetic/ctranspose/CtransposeOGIndexArray.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMAArithmetic.ctranspose;
+
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGArraySuper;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGIndexArray;
+import com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMAArithmetic.transpose.TransposeOGIndexArray;
+import com.opengamma.maths.lowlevelapi.functions.checkers.Catchers;
+
+/**
+ * Conjugate Transposes an OGIndexArray
+ * TODO: At some point consider COW or at least state propagated permutations for things like this?!
+ */
+public final class CtransposeOGIndexArray extends CtransposeAbstract<OGIndexArray> {
+  private static CtransposeOGIndexArray s_instance = new CtransposeOGIndexArray();
+
+  public static CtransposeOGIndexArray getInstance() {
+    return s_instance;
+  }
+
+  private CtransposeOGIndexArray() {
+  }
+  
+  private TransposeOGIndexArray _transpose = TransposeOGIndexArray.getInstance();
+
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public OGArraySuper<Number> ctranspose(OGIndexArray array1) {
+    Catchers.catchNullFromArgList(array1, 1);
+    return _transpose.transpose(array1);
+  }
+
+}
---
+++ projects/OG-Maths/tests/unit/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMAArithmetic/ctranspose/DOGMAOGIndexArrayCtransposeTest.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMAArithmetic.ctranspose;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+import com.opengamma.maths.commonapi.exceptions.MathsExceptionNullPointer;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGIndexArray;
+
+/**
+ * Tests OGIndexArray conjugate transpose
+ */
+public class DOGMAOGIndexArrayCtransposeTest {
+
+  CtransposeOGIndexArray ct = CtransposeOGIndexArray.getInstance();
+
+  int normalRows = 4;
+  int normalCols = 3;
+  int[] _data = new int[] {1, 4, 7, 10, 2, 5, 8, 11, 3, 6, 9, 12 };
+  OGIndexArray array1 = new OGIndexArray(_data, normalRows, normalCols);
+
+
+  int transposedRows = 3;
+  int transposedCols = 4;
+  int[] _dataTransposed = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };  
+  OGIndexArray array1tranposed = new OGIndexArray(_dataTransposed, transposedRows, transposedCols);
+  
+  @Test(expectedExceptions = MathsExceptionNullPointer.class)
+  public void nullInTest() {
+    OGIndexArray tmp = null;
+    ct.ctranspose(tmp);
+  }
+
+  @Test
+  public void testTranspose() {
+    assertTrue(array1tranposed.equals(ct.ctranspose(array1)));
+  }
+
+}
