aaaebb63984fb093db7f019d6b159bd72e879ea4
Stuart Archibald <stuart@opengamma.com>
2012-10-23 12:17:12 +0100

    Added index array transpose

---
+++ projects/OG-Maths/src/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMAArithmetic/transpose/TransposeOGIndexArray.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMAArithmetic.transpose;
+
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGArraySuper;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGIndexArray;
+import com.opengamma.maths.lowlevelapi.functions.checkers.Catchers;
+
+/**
+ * Transposes an OGIndexArray
+ * TODO: At some point consider COW or at least state propagated permutations for things like this?!
+ */
+public final class TransposeOGIndexArray extends TransposeAbstract<OGIndexArray> {
+  private static TransposeOGIndexArray s_instance = new TransposeOGIndexArray();
+
+  public static TransposeOGIndexArray getInstance() {
+    return s_instance;
+  }
+
+  private TransposeOGIndexArray() {
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public OGArraySuper<Number> transpose(OGIndexArray array1) {
+    Catchers.catchNullFromArgList(array1, 1);
+    int rowsArray1 = array1.getNumberOfRows();
+    int columnsArray1 = array1.getNumberOfColumns();
+    int retRows = columnsArray1, retCols = rowsArray1;
+    int[] data = array1.getData();
+    int[] tmp = new int[rowsArray1 * columnsArray1];
+
+    int ir;
+    for (int i = 0; i < columnsArray1; i++) {
+      ir = i * rowsArray1;
+      for (int j = 0; j < rowsArray1; j++) {
+        tmp[j * columnsArray1 + i] = data[ir + j];
+      }
+    }
+    return new OGIndexArray(tmp, retRows, retCols);
+  }
+
+}
---
+++ projects/OG-Maths/tests/unit/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMAArithmetic/transpose/DOGMAOGIndexArrayTransposeTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMAArithmetic.transpose;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+import com.opengamma.maths.commonapi.exceptions.MathsExceptionNullPointer;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGDoubleArray;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGIndexArray;
+
+/**
+ * Tests OGDoubleArray transpose
+ */
+public class DOGMAOGIndexArrayTransposeTest {
+
+  TransposeOGIndexArray t = TransposeOGIndexArray.getInstance();
+
+  int normalRows = 4;
+  int normalCols = 3;
+  int[] _data = new int[] {1, 4, 7, 10, 2, 5, 8, 11, 3, 6, 9, 12 };
+  OGIndexArray array1 = new OGIndexArray(_data, normalRows, normalCols);
+
+
+  int transposedRows = 3;
+  int transposedCols = 4;
+  int[] _dataTransposed = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };  
+  OGIndexArray array1tranposed = new OGIndexArray(_dataTransposed, transposedRows, transposedCols);
+  
+  @Test(expectedExceptions = MathsExceptionNullPointer.class)
+  public void nullInTest() {
+    OGIndexArray tmp = null;
+    t.transpose(tmp);
+  }
+
+  @Test
+  public void testTranspose() {
+    assertTrue(array1tranposed.equals(t.transpose(array1)));
+  }
+
+}
