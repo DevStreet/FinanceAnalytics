9a9a672ab910993e15f95bbcb6830310a0a447a1
Stuart Archibald <stuart@opengamma.com>
2012-10-25 17:26:05 +0100

    Added sparse(dense) function

--- projects/OG-Maths/src/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMASparseUtilities/sparse/SparseOGDoubleArray.java
+++ projects/OG-Maths/src/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMASparseUtilities/sparse/SparseOGDoubleArray.java
@@ -5,9 +5,72 @@
  */
 package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMASparseUtilities.sparse;
 
+import java.util.Arrays;
+
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGArraySuper;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGDoubleArray;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGSparseArray;
+
 /**
- * 
+ * Sparse's a OGDoubleArray
  */
-public class SparseOGDoubleArray {
+public final class SparseOGDoubleArray extends SparseAbstract<OGDoubleArray> {
+
+  private static SparseOGDoubleArray s_instance = new SparseOGDoubleArray();
+
+  public static SparseOGDoubleArray getInstance() {
+    return s_instance;
+  }
+
+  private SparseOGDoubleArray() {
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public OGArraySuper<Number> sparse(OGDoubleArray array1) {
+    final int rows = array1.getNumberOfRows();
+    final int cols = array1.getNumberOfColumns();
+    final double[] data = array1.getData();
+    double[] values;
+    int[] colPtr;
+    int[] rowIdx;
+    int els;
+    //get number of elements
+    els = rows * cols;
+
+    // tmp arrays, in case we get in a fully populated matrix, intelligent design upstream should ensure that this is overkill!
+    double[] dataTmp = new double[els];
+    int[] colPtrTmp = new int[els + 1];
+    int[] rowIdxTmp = new int[els];
+
+    // we need unwind the array m into coordinate form
+    int ptr = 0;
+    int i;
+    int localMaxEntrisInACol;
+    double entry = 0;
+    int ir;
+    for (i = 0; i < cols; i++) {
+      colPtrTmp[i] = ptr;
+      localMaxEntrisInACol = 0;
+      ir = i * rows;
+      for (int j = 0; j < rows; j++) {
+        entry = data[ir + j];
+        if (entry != 0.0e0) {
+          rowIdxTmp[ptr] = j;
+          dataTmp[ptr] = entry;
+          ptr++;
+          localMaxEntrisInACol++;
+        }
+      }
+
+    }
+    colPtrTmp[i] = ptr;
+
+    // return correct 0 to correct length of the vector buffers
+    values = Arrays.copyOfRange(dataTmp, 0, ptr);
+    colPtr = Arrays.copyOfRange(colPtrTmp, 0, i + 1); // yes, the +1 is correct, it allows the computation of the number of elements in the final row!
+    rowIdx = Arrays.copyOfRange(rowIdxTmp, 0, ptr);
+    return new OGSparseArray(colPtr, rowIdx, values, rows, cols);
+  }
 
 }
---
+++ projects/OG-Maths/tests/unit/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMASparseUtilities/sparse/DOGMAOGDoubleArraySparseTest.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMASparseUtilities.sparse;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGSparseArray;
+
+
+/**
+ * tests double ton sparse type
+ */
+public class DOGMAOGDoubleArraySparseTest {
+
+  private static SparseOGSparseArray s_d2s = SparseOGSparseArray.getInstance();
+  private static double[][] denseData = {{      0.0000000000000000,      0.0000000000000000,      0.0000000000000000,      0.0000000000000000,      3.0000000000000000},{      0.0000000000000000,      0.0000000000000000,     10.0000000000000000,      0.0000000000000000,      2.0000000000000000},{     10.0000000000000000,      0.0000000000000000,      4.0000000000000000,      1.0000000000000000,      0.0000000000000000},{      5.0000000000000000,      1.0000000000000000,      8.0000000000000000,      0.0000000000000000,      5.0000000000000000},{     10.0000000000000000,      0.0000000000000000,      0.0000000000000000,      0.0000000000000000,      0.0000000000000000}};
+
+  private static OGSparseArray sparseAnswer = new OGSparseArray(denseData);
+
+  @Test
+  public static void denseToSparseTest() {
+    OGSparseArray p = new OGSparseArray(denseData);
+    assertTrue(sparseAnswer.equals(s_d2s.sparse(p)));
+  }
+
+}
