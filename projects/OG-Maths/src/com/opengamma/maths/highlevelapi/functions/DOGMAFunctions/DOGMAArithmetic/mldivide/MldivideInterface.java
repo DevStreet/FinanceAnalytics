8f417b878ddec4c975e76164937e3e45081b013e
Stuart Archibald <stuart@opengamma.com>
2012-10-30 15:35:02 +0000

    Genericised mldivide interface

--- projects/OG-Maths/src/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMAArithmetic/mldivide/MldivideInterface.java
+++ projects/OG-Maths/src/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMAArithmetic/mldivide/MldivideInterface.java
@@ -8,8 +8,10 @@ package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMAArithmeti
 import com.opengamma.maths.highlevelapi.datatypes.primitive.OGArray;
 
 /**
- * 
+ * mldivide() interface
+ * @param <T> an OGArray type
+ * @param <S> an OGArray type 
  */
 public interface MldivideInterface<T extends OGArray<? extends Number>, S extends OGArray<? extends Number>> {
-  public <U extends Number> OGArray<U > mldivide(T array1, S array2);
+  OGArray<? extends Number> mldivide(T array1, S array2);
 }
