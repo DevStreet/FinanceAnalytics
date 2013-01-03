7b2283c6147a85d2a26295e997333bd4ac8d7396
Stuart Archibald <stuart@opengamma.com>
2013-01-03 14:52:27 +0000

    Added length()

---
+++ projects/OG-Maths/src/main/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMADimensions/length/LengthFunction.java
@@ -0,0 +1,29 @@
+package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMADimensions.length;
+
+import com.opengamma.maths.dogma.engine.DOGMAMethodHook;
+import com.opengamma.maths.dogma.engine.DOGMAMethodLiteral;
+import com.opengamma.maths.dogma.engine.methodhookinstances.arbitrary.Length;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGArray;
+import com.opengamma.maths.lowlevelapi.functions.checkers.Catchers;
+
+/**
+ * does length()
+ */
+@DOGMAMethodHook(provides = Length.class)
+public final class LengthFunction implements Length {
+
+  /**
+   * Returns the "length" of a matrix, this is the length of the largest dimension
+   * @param array1 the array from which the length shall be computed
+   * @return the length of array1
+   */
+  @DOGMAMethodLiteral
+  public int length(OGArray<? extends Number> array1) {
+    Catchers.catchNullFromArgList(array1, 1);
+
+    final int rowsArray1 = array1.getNumberOfRows();
+    final int columnsArray1 = array1.getNumberOfColumns();
+
+    return Math.max(rowsArray1, columnsArray1);
+  }
+}
