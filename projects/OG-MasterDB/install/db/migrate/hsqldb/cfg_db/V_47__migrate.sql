c4fcdc242383113bddf5572db501f9d1ff7ad6cf
Kevin Vella <kevin@opengamma.com>
2013-01-04 17:38:35 +0100

    Remove 'WITHOUT oids' from hsql and sqlserver2008 config db upgrade scripts

--- projects/OG-MasterDB/install/db/migrate/hsqldb/cfg_db/V_47__migrate.sql
+++ projects/OG-MasterDB/install/db/migrate/hsqldb/cfg_db/V_47__migrate.sql
@@ -4,7 +4,7 @@ START TRANSACTION;
   CREATE TABLE cfg_type (
     config_type VARCHAR(255) PRIMARY KEY NOT NULL,
     CONSTRAINT cfg_fk_type2type FOREIGN KEY (config_type) REFERENCES cfg_type (config_type)
-  ) WITHOUT oids;
+  );
   
   INSERT INTO cfg_type (config_type) (SELECT DISTINCT config_type FROM cfg_config);
 COMMIT;
--- projects/OG-MasterDB/install/db/migrate/sqlserver2008/cfg_db/V_47__migrate.sql
+++ projects/OG-MasterDB/install/db/migrate/sqlserver2008/cfg_db/V_47__migrate.sql
@@ -5,7 +5,7 @@ BEGIN TRAN;
   CREATE TABLE cfg_type (
     config_type VARCHAR(255) PRIMARY KEY NOT NULL,
     CONSTRAINT cfg_fk_type2type FOREIGN KEY (config_type) REFERENCES cfg_type (config_type)
-  ) WITHOUT oids;
+  );
   
   INSERT INTO cfg_type (config_type) (SELECT DISTINCT config_type FROM cfg_config);
 COMMIT;
