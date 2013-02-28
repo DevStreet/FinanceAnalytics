cdf273b6f8bb379a979c9392795111898057b972
Stuart Archibald <stuart@opengamma.com>
2013-02-28 13:44:38 +0000

    updated a couple of slatec functions

--- projects/OG-Maths/src/main/java/com/opengamma/maths/lowlevelapi/slatec/fnlib/ZASINH.java
+++ projects/OG-Maths/src/main/java/com/opengamma/maths/lowlevelapi/slatec/fnlib/ZASINH.java
@@ -6,10 +6,18 @@
 package com.opengamma.maths.lowlevelapi.slatec.fnlib;
 
 /**
- * 
+ * ZASINH(Z) computes the trigonometric inverse hyperbolic sine of the complex argument.
+ * This code is an approximate translation of the equivalent function in the "Public Domain" code from SLATEC, see:
+ * See http://www.netlib.org/slatec/fnlib/casinh.f
+ * Major difference is that this is in double precision arithmetic
  */
 public class ZASINH {
 
+  /**
+   * Computes the trigonometric inverse hyperbolic sine at position 'x'
+   * @param x the position at which to compute the trigonometric inverse hyperbolic sine 
+   * @return the value of the trigonometric inverse hyperbolic sine at position 'x'
+   */
   public static double[] zasinh(double[] x) {
     double[] tmp = new double[2];
     tmp[0] = -x[1];
