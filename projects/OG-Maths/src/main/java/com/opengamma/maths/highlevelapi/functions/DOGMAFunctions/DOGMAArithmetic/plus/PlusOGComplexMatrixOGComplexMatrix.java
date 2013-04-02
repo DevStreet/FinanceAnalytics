af97afc6acb479e8cdc454d7a440cb71bda8fa15
Stuart Archibald <stuart@opengamma.com>
2013-04-02 12:13:34 +0100

    Added complex+complex

--- projects/OG-Maths/src/main/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMAArithmetic/plus/PlusOGComplexMatrixOGComplexMatrix.java
+++ projects/OG-Maths/src/main/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMAArithmetic/plus/PlusOGComplexMatrixOGComplexMatrix.java
@@ -8,7 +8,7 @@ package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMAArithmeti
 import com.opengamma.maths.dogma.engine.DOGMAMethodHook;
 import com.opengamma.maths.dogma.engine.methodhookinstances.infix.Plus;
 import com.opengamma.maths.highlevelapi.datatypes.primitive.OGComplexMatrix;
-import com.opengamma.maths.lowlevelapi.exposedapi.BLAS;
+import com.opengamma.maths.lowlevelapi.exposedapi.EasyIZY;
 import com.opengamma.maths.lowlevelapi.functions.checkers.Catchers;
 
 /**
@@ -17,8 +17,6 @@ import com.opengamma.maths.lowlevelapi.functions.checkers.Catchers;
  */
 @DOGMAMethodHook(provides = Plus.class)
 public class PlusOGComplexMatrixOGComplexMatrix implements Plus<OGComplexMatrix, OGComplexMatrix, OGComplexMatrix> {
-
-  private BLAS _localblas = new BLAS();
 
   @Override
   public OGComplexMatrix eval(OGComplexMatrix array1, OGComplexMatrix array2) {
@@ -34,25 +32,13 @@ public class PlusOGComplexMatrixOGComplexMatrix implements Plus<OGComplexMatrix,
     if (rowsArray1 == 1 && columnsArray1 == 1) { // array 1 is scalar, i.e. X+iY
       n = array2.getData().length;
       tmp = new double[n];
-      System.arraycopy(array2.getData(), 0, tmp, 0, n);
-      final double singleRealDouble = array1.getData()[0];
-      final double singleImagDouble = array1.getData()[1];
-      for (int i = 0; i < n; i += 2) {
-        tmp[i] += singleRealDouble;
-        tmp[i + 1] += singleImagDouble;
-      }
+      EasyIZY.vz_addx(array2.getData(), array1.getData(), tmp);
       retRows = rowsArray2;
       retCols = columnsArray2;
     } else if (rowsArray2 == 1 && columnsArray2 == 1) {
       n = array1.getData().length;
       tmp = new double[n];
-      System.arraycopy(array1.getData(), 0, tmp, 0, n);
-      final double singleRealDouble = array2.getData()[0];
-      final double singleImagDouble = array2.getData()[1];
-      for (int i = 0; i < n; i += 2) {
-        tmp[i] += singleRealDouble;
-        tmp[i + 1] += singleImagDouble;
-      }
+      EasyIZY.vz_addx(array1.getData(), array2.getData(), tmp);
       retRows = rowsArray1;
       retCols = columnsArray1;
     } else {
@@ -60,8 +46,7 @@ public class PlusOGComplexMatrixOGComplexMatrix implements Plus<OGComplexMatrix,
       Catchers.catchBadCommute(columnsArray1, "columns in first array", columnsArray2, "columns in second array");
       n = array1.getData().length;
       tmp = new double[n];
-      System.arraycopy(array1.getData(), 0, tmp, 0, n);
-      _localblas.daxpy(n, 1, array2.getData(), 1, tmp, 1);
+      EasyIZY.vz_add(array1.getData(), array2.getData(), tmp);
       retRows = rowsArray1;
       retCols = columnsArray1;
     }
