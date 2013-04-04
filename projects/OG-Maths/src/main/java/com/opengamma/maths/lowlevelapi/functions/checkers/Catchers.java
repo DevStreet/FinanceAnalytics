8561d14720826eae11de5c02714d7b9762679771
Stuart Archibald <stuart@opengamma.com>
2013-04-04 15:22:07 +0100

    Complaint string was incorrect

--- projects/OG-Maths/src/main/java/com/opengamma/maths/lowlevelapi/functions/checkers/Catchers.java
+++ projects/OG-Maths/src/main/java/com/opengamma/maths/lowlevelapi/functions/checkers/Catchers.java
@@ -818,7 +818,7 @@ public class Catchers {
    */
   public static void catchBadCommute(int dim1, String varName1, int dim2, String varName2) {
     if (dim1 != dim2) {
-      throw new MathsExceptionNonConformance(varName1 + " (" + dim1 + ") and " + varName2 + " (" + dim1 + ") do not commute. STOPPING");
+      throw new MathsExceptionNonConformance(varName1 + " (" + dim1 + ") and " + varName2 + " (" + dim2 + ") do not commute. STOPPING");
     }
   }
 
