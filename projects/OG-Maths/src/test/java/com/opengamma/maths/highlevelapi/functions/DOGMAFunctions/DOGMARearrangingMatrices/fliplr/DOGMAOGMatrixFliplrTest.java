658b0a881ed7367d00d5d1ac4de9c02a45694ebe
Stuart Archibald <stuart@opengamma.com>
2013-04-05 11:07:26 +0100

    matrix flipping functions

---
+++ projects/OG-Maths/src/test/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMARearrangingMatrices/fliplr/DOGMAOGMatrixFliplrTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMARearrangingMatrices.fliplr;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import com.opengamma.analytics.math.statistics.distribution.fnlib.D1MACH;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGArray;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGMatrix;
+import com.opengamma.util.test.TestGroup;
+
+/**
+ * tests fliplr on OGMatrix
+ */
+@Test(groups = TestGroup.UNIT)
+public class DOGMAOGMatrixFliplrTest {
+
+  double[][] A = new double[][] { {1., 2., 3. }, {4., 5., 6. }, {7., 8., 9. }, {10., 11., 12. } };
+  double[][] V = new double[][] { {1. }, {2. }, {3. } };
+  double[][] H = new double[][] {{1., 2., 3. } };
+
+  OGMatrix Amatrix = new OGMatrix(A);
+  OGMatrix Vmatrix = new OGMatrix(V);
+  OGMatrix Hmatrix = new OGMatrix(H);
+
+  FliplrOGMatrix fliplr = new FliplrOGMatrix();
+
+  @Test
+  public void matrixfliplrTest() {
+    OGMatrix answer = new OGMatrix(new double[][] { {3., 2., 1. }, {6., 5., 4. }, {9., 8., 7. }, {12., 11., 10. } });
+    OGArray<? extends Number> tmp = fliplr.eval(Amatrix);
+    assertTrue(answer.fuzzyequals(tmp, 10 * D1MACH.four()));
+  }
+
+  @Test
+  public void vvectorfliplrTest() {
+    OGMatrix answer = Vmatrix;
+    OGArray<? extends Number> tmp = fliplr.eval(Vmatrix);
+    assertTrue(answer.fuzzyequals(tmp, 10 * D1MACH.four()));
+  }
+
+  @Test
+  public void hvectorfliplrTest() {
+    OGMatrix answer = new OGMatrix(new double[][] {{3., 2., 1. } });
+    OGArray<? extends Number> tmp = fliplr.eval(Hmatrix);
+    assertTrue(answer.fuzzyequals(tmp, 10 * D1MACH.four()));
+  }
+
+}
---
+++ projects/OG-Maths/src/test/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMARearrangingMatrices/flipud/DOGMAOGMatrixFlipudTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMARearrangingMatrices.flipud;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import com.opengamma.analytics.math.statistics.distribution.fnlib.D1MACH;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGArray;
+import com.opengamma.maths.highlevelapi.datatypes.primitive.OGMatrix;
+import com.opengamma.util.test.TestGroup;
+
+/**
+ * tests flipud on OGMatrix
+ */
+@Test(groups = TestGroup.UNIT)
+public class DOGMAOGMatrixFlipudTest {
+
+  double[][] A = new double[][] { {1., 2., 3. }, {4., 5., 6. }, {7., 8., 9. }, {10., 11., 12. } };
+  double[][] V = new double[][] { {1. }, {2. }, {3. } };
+  double[][] H = new double[][] {{1., 2., 3. } };
+
+  OGMatrix Amatrix = new OGMatrix(A);
+  OGMatrix Vmatrix = new OGMatrix(V);
+  OGMatrix Hmatrix = new OGMatrix(H);
+
+  FlipudOGMatrix flipud = new FlipudOGMatrix();
+
+  @Test
+  public void matrixflipudTest() {
+    OGMatrix answer = new OGMatrix(new double[][] { {10., 11., 12. }, {7., 8., 9. }, {4., 5., 6. }, {1., 2., 3. } });
+    OGArray<? extends Number> tmp = flipud.eval(Amatrix);
+    assertTrue(answer.fuzzyequals(tmp, 10 * D1MACH.four()));
+  }
+
+  @Test
+  public void vvectorflipudTest() {
+    OGMatrix answer = new OGMatrix(new double[][] { {3. }, {2. }, {1. } });
+    OGArray<? extends Number> tmp = flipud.eval(Vmatrix);
+    assertTrue(answer.fuzzyequals(tmp, 10 * D1MACH.four()));
+  }
+
+  @Test
+  public void hvectorflipudTest() {
+    OGMatrix answer = Hmatrix;
+    OGArray<? extends Number> tmp = flipud.eval(Hmatrix);
+    assertTrue(answer.fuzzyequals(tmp, 10 * D1MACH.four()));
+  }
+
+}
