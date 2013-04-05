ba1f793a35c5a2e3fbde425ab3be18e30fb6d168
Stuart Archibald <stuart@opengamma.com>
2013-04-05 11:05:49 +0100

    updated method hooks

--- projects/OG-Maths/src/main/java/com/opengamma/maths/dogma/engine/methodhookinstances/unary/Horzcat.java
+++ projects/OG-Maths/src/main/java/com/opengamma/maths/dogma/engine/methodhookinstances/unary/Horzcat.java
@@ -3,7 +3,7 @@
  * 
  * Please see distribution for license.
  */
-package com.opengamma.maths.dogma.engine.methodhookinstances.binary;
+package com.opengamma.maths.dogma.engine.methodhookinstances.unary;
 
 import com.opengamma.maths.dogma.engine.language.InfixOperator;
 import com.opengamma.maths.highlevelapi.datatypes.primitive.OGArray;
