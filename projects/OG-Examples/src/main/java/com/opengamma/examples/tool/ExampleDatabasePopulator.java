e32ef189631eb55af2427c972e6674e3d5927b2e
Jonathan Senior <jonathan@opengamma.com>
2013-04-11 16:15:45 +0100

    Remove duplicate operation to populate yield curves

--- projects/OG-Examples/src/main/java/com/opengamma/examples/tool/ExampleDatabasePopulator.java
+++ projects/OG-Examples/src/main/java/com/opengamma/examples/tool/ExampleDatabasePopulator.java
@@ -111,7 +111,6 @@ public class ExampleDatabasePopulator extends AbstractTool<ToolContext> {
     loadCurveCalculationConfigurations();
     loadDefaultVolatilityCubeDefinition();
     loadTimeSeriesRating();
-    loadYieldCurves();
     loadSimulatedHistoricalData();
     loadMultiCurrencySwapPortfolio();
     loadAUDSwapPortfolio();
@@ -219,16 +218,6 @@ public class ExampleDatabasePopulator extends AbstractTool<ToolContext> {
     try {
       final ExampleTimeSeriesRatingLoader timeSeriesRatingLoader = new ExampleTimeSeriesRatingLoader();
       timeSeriesRatingLoader.run(getToolContext());
-      log.done();
-    } catch (final RuntimeException t) {
-      log.fail(t);
-    }
-  }
-
-  private void loadYieldCurves() {
-    final Log log = new Log("Creating yield curve definitions");
-    try {
-      YieldCurveConfigPopulator.populateSyntheticCurveConfigMaster(getToolContext().getConfigMaster());
       log.done();
     } catch (final RuntimeException t) {
       log.fail(t);
