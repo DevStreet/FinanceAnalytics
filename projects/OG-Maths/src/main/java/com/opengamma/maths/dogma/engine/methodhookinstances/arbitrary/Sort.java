fb515ab6c74a739398679292b720de79672c80c7
Stuart Archibald <stuart@opengamma.com>
2013-04-24 16:07:13 +0100

    updated sort to reference new location of ParallelArrayBinarySort, added missing annotation class

---
+++ projects/OG-Maths/src/main/java/com/opengamma/maths/dogma/engine/methodhookinstances/arbitrary/Sort.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
+ * 
+ * Please see distribution for license.
+ */
+package com.opengamma.maths.dogma.engine.methodhookinstances.arbitrary;
+
+import com.opengamma.maths.dogma.engine.language.ArbitraryFunction;
+
+/**
+ * @param <R> ret
+ */
+public interface Sort extends ArbitraryFunction {
+
+}
