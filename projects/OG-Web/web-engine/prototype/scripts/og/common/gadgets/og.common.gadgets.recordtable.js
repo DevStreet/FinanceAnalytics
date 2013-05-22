cfe788446bc99f29f2927f32a9edcb91f15586a7
Antonio Greco <agreco13@gmail.com>
2013-05-22 01:06:08 +0100

    WIP RecordTable field/data types arrays

--- projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.recordtable.js
+++ projects/OG-Web/web-engine/prototype/scripts/og/common/gadgets/og.common.gadgets.recordtable.js
@@ -9,7 +9,9 @@ $.register_module({
         var RecordTable = function (config) {
             if (!config) og.dev.warn('og.common.gadgets.RecordTable: Missing param [config] to constructor.');
 
-            var form, data = config.data, headers = data.headers, rows = data.rows, field_types = {},
+            var form, data = config.data, headers = data.headers, rows = data.rows,
+                // TODO AG: use Object.clone on headers array and splice out unused props
+                data_types = headers.map(function (header) {return {id: header.id, data_type: header.data_type};}),
                 actions = {
                     cell: [ {name: 'edit'} ],
                     row: [
@@ -19,28 +21,23 @@ $.register_module({
                         {name: 'revert'}
                     ],
                     table: [ {name: 'add'} ]
-                };
-
-            field_types = rows.map(function (row) {
-                return {
-                    row: row.id,
-                    cols: row.cells.map(function (cell) { // Refactor
-                        return {
-                            id: cell.id,
-                            data_type: headers.filter(function (header) {
-                                if (header.id === cell.id) return header.data_type;
-                            })
-                        };
-                    })
-                };
-            });
-
-            console.log(field_types);
+                },
+                field_types = rows.map(function (row) {
+                    return {
+                        row: row.id,
+                        cols: row.cells.map(function (cell, idx) {
+                            return {
+                                id: cell.id,
+                                data_type: data_types[idx].id === cell.id ? data_types[idx].data_type : void 0
+                            };
+                        })
+                    };
+                });
 
             var add_handler = function () {
                 new form.Block({
                     module: 'og.views.gadgets.recordtable.row_tash',
-                    extras: {'fields': headers.map(create_field_type)}
+                    extras: {'fields': field_types.map(create_field_type)}
                 }).html(function (html) {
                     $('.OG-gadget-recordtable tbody').append(html);
                 });
@@ -50,19 +47,16 @@ $.register_module({
                 var template = {};
                 switch (field.type) {
                     case 'string':
-                    case 'number': {
+                    case 'number':
                         template.html = '<input type="text" val="{{val}}" />';
                         break;
-                    }
-                    case 'array': {
+                    case 'array':
                         template.html = '<select>{{#each opts}}<option>{{this.val}}</option>{{/each}}</select>';
                         break;
-                    }
-                    case 'boolean': {
+                    case 'boolean':
                         template.html = '<input name="'+field.id+'" type="radio" val="true" /> '+
                                         '<input name="'+field.id+'" type="radio" val="true" />';
                         break;
-                    }
                 }
                 return template;
             };
