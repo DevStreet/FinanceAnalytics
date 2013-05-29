27d3b2f1ffc1fea8bc505e91ec931418c580657e
Stuart Archibald <stuart@opengamma.com>
2013-05-29 09:32:22 +0100

    added missing comments

--- projects/OG-Maths/src/main/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMARearrangingMatrices/sort/SortCompute.java
+++ projects/OG-Maths/src/main/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMARearrangingMatrices/sort/SortCompute.java
@@ -6,14 +6,23 @@
 package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMARearrangingMatrices.sort;
 
 /**
- * 
+ * Enum to describe what the sort() function should be computing
  */
 public enum SortCompute {
 
+  /**
+   * Compute the sorted "values"
+   */
   values,
 
+  /**
+   * Compute the sorted order of the keys based on "values"
+   */
   keys,
 
+  /**
+   * Compute by sorting the "values" and permuting the "keys" by the same order
+   */
   valuesAndKeys
 
 }
