929fe5b6438f06efead1e0cbb73eb89547de389f
Stuart Archibald <stuart@opengamma.com>
2013-05-29 09:34:00 +0100

    Added missing copyright

--- projects/OG-Maths/src/main/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMADimensions/length/LengthFunction.java
+++ projects/OG-Maths/src/main/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMADimensions/length/LengthFunction.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2013 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
 package com.opengamma.maths.highlevelapi.functions.DOGMAFunctions.DOGMADimensions.length;
 
 import com.opengamma.maths.dogma.engine.DOGMAMethodHook;
