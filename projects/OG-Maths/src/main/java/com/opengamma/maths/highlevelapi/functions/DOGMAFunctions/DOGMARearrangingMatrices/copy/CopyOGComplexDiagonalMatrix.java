8b78f9df3f420ac18970a4f46750604a2078bb1c
Stuart Archibald <stuart@opengamma.com>
2013-07-09 16:32:56 +0100

    [MAT-88] forgot this fix

--- projects/OG-Maths/src/main/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMARearrangingMatrices/copy/CopyOGComplexDiagonalMatrix.java
+++ projects/OG-Maths/src/main/java/com/opengamma/maths/highlevelapi/functions/DOGMAFunctions/DOGMARearrangingMatrices/copy/CopyOGComplexDiagonalMatrix.java
@@ -16,12 +16,19 @@ import com.opengamma.maths.highlevelapi.datatypes.primitive.OGComplexDiagonalMat
 public final class CopyOGComplexDiagonalMatrix implements Copy<OGComplexDiagonalMatrix, OGComplexDiagonalMatrix> {
   @Override
   public OGComplexDiagonalMatrix eval(OGComplexDiagonalMatrix array1) {
-    final int n = array1.getData().length;
-    final double[] tmp = new double[n];
+    final double[] data = array1.getData();
+    final int n = data.length;
     final int rows = array1.getNumberOfRows();
     final int cols = array1.getNumberOfColumns();
-    System.arraycopy(array1.getData(), 0, tmp, 0, n);
-    return new OGComplexDiagonalMatrix(tmp, rows, cols);
+    int len = n / 2;
+    double[] real = new double[len];
+    double[] imag = new double[len];
+    int jmp = 0;
+    for (int i = 0; i < len; i++) {
+      real[i] = data[jmp];
+      imag[i] = data[jmp + 1];
+      jmp += 2;
+    }
+    return new OGComplexDiagonalMatrix(real, imag, rows, cols);
   }
-
 }
