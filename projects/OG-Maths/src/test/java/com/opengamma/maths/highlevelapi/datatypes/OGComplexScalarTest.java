5e1bce1fdcbb749cc10aace191ac950a7051d59a
Stuart Archibald <stuart@opengamma.com>
2013-07-10 15:33:10 +0100

    [MAT-89] WIP X massive refactor on data type test packages

--- projects/OG-Maths/src/test/java/com/opengamma/maths/highlevelapi/datatypes/OGComplexScalarTest.java
+++ projects/OG-Maths/src/test/java/com/opengamma/maths/highlevelapi/datatypes/OGComplexScalarTest.java
@@ -3,7 +3,7 @@
  * 
  * Please see distribution for license.
  */
-package com.opengamma.maths.highlevelapi.datatypes.primitive;
+package com.opengamma.maths.highlevelapi.datatypes;
 
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
