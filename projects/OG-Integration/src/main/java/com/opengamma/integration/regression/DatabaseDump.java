764929df7c9a8aa8ba9d65257bef3e810aba72e0
Tim Raggatt <tim@opengamma.com>
2013-12-11 17:52:39 +0000

    [PLAT-5023] Removed unnecessary RegressionIdPreProcessor; CalculationResults now uses ObjectId.toString() as default regression id if not already populated; Added filtering so only records accessed are persisted in dump.

--- projects/OG-Integration/src/main/java/com/opengamma/integration/regression/DatabaseDump.java
+++ projects/OG-Integration/src/main/java/com/opengamma/integration/regression/DatabaseDump.java
@@ -5,10 +5,13 @@
  */
 package com.opengamma.integration.regression;
 
+import static com.google.common.collect.Iterators.filter;
+import static com.google.common.collect.Iterators.transform;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 
 import net.sf.saxon.event.StreamWriterToReceiver;
@@ -25,44 +28,60 @@ import org.fudgemsg.wire.xml.FudgeXMLStreamWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Function;
 import com.google.common.base.Throwables;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.opengamma.OpenGammaRuntimeException;
 import com.opengamma.core.config.impl.ConfigItem;
+import com.opengamma.core.marketdatasnapshot.impl.ManageableMarketDataSnapshot;
 import com.opengamma.id.ObjectId;
 import com.opengamma.id.UniqueIdentifiable;
 import com.opengamma.id.VersionCorrection;
 import com.opengamma.integration.server.RemoteServer;
+import com.opengamma.master.config.ConfigDocument;
 import com.opengamma.master.config.ConfigMaster;
 import com.opengamma.master.config.ConfigSearchRequest;
-import com.opengamma.master.config.ConfigSearchResult;
+import com.opengamma.master.config.impl.ConfigSearchIterator;
+import com.opengamma.master.exchange.ExchangeDocument;
 import com.opengamma.master.exchange.ExchangeMaster;
 import com.opengamma.master.exchange.ExchangeSearchRequest;
-import com.opengamma.master.exchange.ExchangeSearchResult;
+import com.opengamma.master.exchange.ManageableExchange;
+import com.opengamma.master.exchange.impl.ExchangeSearchIterator;
+import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesInfoDocument;
 import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesInfoSearchRequest;
-import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesInfoSearchResult;
 import com.opengamma.master.historicaltimeseries.HistoricalTimeSeriesMaster;
 import com.opengamma.master.historicaltimeseries.ManageableHistoricalTimeSeries;
 import com.opengamma.master.historicaltimeseries.ManageableHistoricalTimeSeriesInfo;
+import com.opengamma.master.historicaltimeseries.impl.HistoricalTimeSeriesInfoSearchIterator;
+import com.opengamma.master.holiday.HolidayDocument;
 import com.opengamma.master.holiday.HolidayMaster;
 import com.opengamma.master.holiday.HolidaySearchRequest;
-import com.opengamma.master.holiday.HolidaySearchResult;
+import com.opengamma.master.holiday.ManageableHoliday;
+import com.opengamma.master.holiday.impl.HolidaySearchIterator;
+import com.opengamma.master.marketdatasnapshot.MarketDataSnapshotDocument;
 import com.opengamma.master.marketdatasnapshot.MarketDataSnapshotMaster;
 import com.opengamma.master.marketdatasnapshot.MarketDataSnapshotSearchRequest;
-import com.opengamma.master.marketdatasnapshot.MarketDataSnapshotSearchResult;
+import com.opengamma.master.marketdatasnapshot.impl.MarketDataSnapshotSearchIterator;
+import com.opengamma.master.orgs.ManageableOrganization;
+import com.opengamma.master.orgs.OrganizationDocument;
 import com.opengamma.master.orgs.OrganizationMaster;
 import com.opengamma.master.orgs.OrganizationSearchRequest;
-import com.opengamma.master.orgs.OrganizationSearchResult;
+import com.opengamma.master.orgs.impl.OrganizationSearchIterator;
+import com.opengamma.master.portfolio.ManageablePortfolio;
+import com.opengamma.master.portfolio.PortfolioDocument;
 import com.opengamma.master.portfolio.PortfolioMaster;
 import com.opengamma.master.portfolio.PortfolioSearchRequest;
-import com.opengamma.master.portfolio.PortfolioSearchResult;
+import com.opengamma.master.portfolio.impl.PortfolioSearchIterator;
+import com.opengamma.master.position.ManageablePosition;
+import com.opengamma.master.position.PositionDocument;
 import com.opengamma.master.position.PositionMaster;
 import com.opengamma.master.position.PositionSearchRequest;
-import com.opengamma.master.position.PositionSearchResult;
+import com.opengamma.master.position.impl.PositionSearchIterator;
+import com.opengamma.master.security.ManageableSecurity;
+import com.opengamma.master.security.SecurityDocument;
 import com.opengamma.master.security.SecurityMaster;
 import com.opengamma.master.security.SecuritySearchRequest;
-import com.opengamma.master.security.SecuritySearchResult;
+import com.opengamma.master.security.impl.SecuritySearchIterator;
 import com.opengamma.util.ArgumentChecker;
 import com.opengamma.util.fudgemsg.OpenGammaFudgeContext;
 
@@ -84,28 +103,57 @@ import com.opengamma.util.fudgemsg.OpenGammaFudgeContext;
   private final ExchangeMaster _exchangeMaster;
   private final MarketDataSnapshotMaster _snapshotMaster;
   private final OrganizationMaster _organizationMaster;
+  
+  private final MasterFilterManager _masterFilterManager;
+  
   private final IdMappings _idMappings;
   private final FudgeContext _ctx = new FudgeContext(OpenGammaFudgeContext.getInstance());
   private final FudgeSerializer _serializer = new FudgeSerializer(OpenGammaFudgeContext.getInstance());
 
   private int _nextId;
 
-  /* package */ DatabaseDump(String outputDir,
-                             SecurityMaster securityMaster,
-                             PositionMaster positionMaster,
-                             PortfolioMaster portfolioMaster,
-                             ConfigMaster configMaster,
-                             HistoricalTimeSeriesMaster timeSeriesMaster,
-                             HolidayMaster holidayMaster,
-                             ExchangeMaster exchangeMaster,
-                             MarketDataSnapshotMaster snapshotMaster,
-                             OrganizationMaster organizationMaster) {
+
+  DatabaseDump(String outputDir,
+      SecurityMaster securityMaster,
+      PositionMaster positionMaster,
+      PortfolioMaster portfolioMaster,
+      ConfigMaster configMaster,
+      HistoricalTimeSeriesMaster timeSeriesMaster,
+      HolidayMaster holidayMaster,
+      ExchangeMaster exchangeMaster,
+      MarketDataSnapshotMaster snapshotMaster,
+      OrganizationMaster organizationMaster) {
+    this(outputDir,
+        securityMaster,
+        positionMaster,
+        portfolioMaster,
+        configMaster,
+        timeSeriesMaster,
+        holidayMaster,
+        exchangeMaster,
+        snapshotMaster,
+        organizationMaster,
+        MasterFilterManager.alwaysTrue());
+  }
+  
+  DatabaseDump(String outputDir,
+      SecurityMaster securityMaster,
+      PositionMaster positionMaster,
+      PortfolioMaster portfolioMaster,
+      ConfigMaster configMaster,
+      HistoricalTimeSeriesMaster timeSeriesMaster,
+      HolidayMaster holidayMaster,
+      ExchangeMaster exchangeMaster,
+      MarketDataSnapshotMaster snapshotMaster,
+      OrganizationMaster organizationMaster,
+      MasterFilterManager masterFilterManager) {
     ArgumentChecker.notNull(securityMaster, "securityMaster");
     ArgumentChecker.notNull(outputDir, "outputDir");
     ArgumentChecker.notNull(positionMaster, "positionMaster");
     ArgumentChecker.notNull(portfolioMaster, "portfolioMaster");
     ArgumentChecker.notNull(configMaster, "configMaster");
     ArgumentChecker.notNull(timeSeriesMaster, "timeSeriesMaster");
+    ArgumentChecker.notNull(masterFilterManager, "masterFilterManager");
     _organizationMaster = organizationMaster;
     _snapshotMaster = snapshotMaster;
     _exchangeMaster = exchangeMaster;
@@ -114,8 +162,11 @@ import com.opengamma.util.fudgemsg.OpenGammaFudgeContext;
     _positionMaster = positionMaster;
     _portfolioMaster = portfolioMaster;
     _configMaster = configMaster;
-    _outputDir = new File(outputDir);
     _securityMaster = securityMaster;
+    _masterFilterManager = masterFilterManager;
+
+    _outputDir = new File(outputDir);
+
     ConfigItem<IdMappings> mappingsConfigItem = RegressionUtils.loadIdMappings(_configMaster);
     if (mappingsConfigItem != null) {
       _idMappings = mappingsConfigItem.getValue();
@@ -182,58 +233,60 @@ import com.opengamma.util.fudgemsg.OpenGammaFudgeContext;
   }
 
   private Map<ObjectId, Integer> writeSecurities() throws IOException {
-    SecuritySearchResult result = _securityMaster.search(new SecuritySearchRequest());
-    return writeToDirectory(result.getSecurities(), "securities", "sec");
+    SecuritySearchIterator searchIterator = new SecuritySearchIterator(_securityMaster, new SecuritySearchRequest());
+    Iterator<SecurityDocument> filteredDocuments = filter(searchIterator, _masterFilterManager.getSecurityFilter());
+    return writeToDirectory(transform(filteredDocuments, new SecurityTransformer()), "securities", "sec");
   }
 
   private Map<ObjectId, Integer> writePositions() throws IOException {
-    PositionSearchResult result = _positionMaster.search(new PositionSearchRequest());
-    return writeToDirectory(result.getPositions(), "positions", "pos");
+    PositionSearchIterator searchIterator = new PositionSearchIterator(_positionMaster, new PositionSearchRequest());
+    Iterator<PositionDocument> filteredDocuments = filter(searchIterator, _masterFilterManager.getPositionFilter());
+    return writeToDirectory(transform(filteredDocuments, new PositionTransformer()), "positions", "pos");
   }
 
   private Map<ObjectId, Integer> writeConfig() throws IOException {
-    ConfigSearchResult<Object> result = _configMaster.search(new ConfigSearchRequest<>(Object.class));
-    return writeToDirectory(result.getValues(), "configs", "cfg");
+    ConfigSearchIterator<Object> searchIterator = new ConfigSearchIterator<>(_configMaster, new ConfigSearchRequest<>(Object.class));
+    Iterator<ConfigDocument> filteredDocuments = filter(searchIterator, _masterFilterManager.getConfigFilter());
+    return writeToDirectory(transform(filteredDocuments, new ConfigTransformer()), "configs", "cfg");
   }
 
   private Map<ObjectId, Integer> writePortfolios() throws IOException {
-    PortfolioSearchResult result = _portfolioMaster.search(new PortfolioSearchRequest());
-    return writeToDirectory(result.getPortfolios(), "portfolios", "prt");
+    PortfolioSearchIterator searchIterator = new PortfolioSearchIterator(_portfolioMaster, new PortfolioSearchRequest());
+    Iterator<PortfolioDocument> filteredDocuments = filter(searchIterator, _masterFilterManager.getPortfolioFilter());
+    return writeToDirectory(transform(filteredDocuments, new PortfolioTransformer()), "portfolios", "prt");
   }
 
   private Map<ObjectId, Integer> writeTimeSeries() throws IOException {
-    List<TimeSeriesWithInfo> objects = Lists.newArrayList();
-    HistoricalTimeSeriesInfoSearchResult infoResult = _timeSeriesMaster.search(new HistoricalTimeSeriesInfoSearchRequest());
-    for (ManageableHistoricalTimeSeriesInfo info : infoResult.getInfoList()) {
-      ManageableHistoricalTimeSeries timeSeries =
-          _timeSeriesMaster.getTimeSeries(info.getTimeSeriesObjectId(), VersionCorrection.LATEST);
-      TimeSeriesWithInfo timeSeriesWithInfo = new TimeSeriesWithInfo(info, timeSeries);
-      objects.add(timeSeriesWithInfo);
-    }
-    return writeToDirectory(objects, "timeseries", "hts");
+    HistoricalTimeSeriesInfoSearchIterator searchIterator = new HistoricalTimeSeriesInfoSearchIterator(_timeSeriesMaster, new HistoricalTimeSeriesInfoSearchRequest());
+    Iterator<HistoricalTimeSeriesInfoDocument> filteredDocuments = filter(searchIterator, _masterFilterManager.getHtsFilter());
+    return writeToDirectory(transform(filteredDocuments, new TimeSeriesTransformer()), "timeseries", "hts");
   }
 
   private Map<ObjectId, Integer> writeHolidays() throws IOException {
-    HolidaySearchResult result = _holidayMaster.search(new HolidaySearchRequest());
-    return writeToDirectory(result.getHolidays(), "holidays", "hol");
+    HolidaySearchIterator searchIterator = new HolidaySearchIterator(_holidayMaster, new HolidaySearchRequest());
+    Iterator<HolidayDocument> filteredDocuments = filter(searchIterator, _masterFilterManager.getHolidayFilter());
+    return writeToDirectory(transform(filteredDocuments, new HolidayTransformer()), "holidays", "hol");
   }
 
   private Map<ObjectId, Integer> writeExchanges() throws IOException {
-    ExchangeSearchResult result = _exchangeMaster.search(new ExchangeSearchRequest());
-    return writeToDirectory(result.getExchanges(), "exchanges", "exg");
+    ExchangeSearchIterator searchIterator = new ExchangeSearchIterator(_exchangeMaster, new ExchangeSearchRequest());
+    Iterator<ExchangeDocument> filteredDocuments = filter(searchIterator, _masterFilterManager.getExchangeFilter());
+    return writeToDirectory(transform(filteredDocuments, new ExchangeTransformer()), "exchanges", "exg");
   }
 
   private Map<ObjectId, Integer> writeSnapshots() throws IOException {
-    MarketDataSnapshotSearchResult result = _snapshotMaster.search(new MarketDataSnapshotSearchRequest());
-    return writeToDirectory(result.getSnapshots(), "snapshots", "snp");
+    MarketDataSnapshotSearchIterator searchIterator = new MarketDataSnapshotSearchIterator(_snapshotMaster, new MarketDataSnapshotSearchRequest());
+    Iterator<MarketDataSnapshotDocument> filteredDocuments = filter(searchIterator, _masterFilterManager.getMarketDataSnapshotFilter());
+    return writeToDirectory(transform(filteredDocuments, new SnapshotTransformer()), "snapshots", "snp");
   }
 
   private Map<ObjectId, Integer> writeOrganizations() throws IOException {
-    OrganizationSearchResult result = _organizationMaster.search(new OrganizationSearchRequest());
-    return writeToDirectory(result.getOrganizations(), "organizations", "org");
+    OrganizationSearchIterator searchIterator = new OrganizationSearchIterator(_organizationMaster, new OrganizationSearchRequest());
+    Iterator<OrganizationDocument> filteredDocuments = filter(searchIterator, _masterFilterManager.getOrganizationFilter());
+    return writeToDirectory(transform(filteredDocuments, new OrganizationTransformer()), "organizations", "org");
   }
 
-  private Map<ObjectId, Integer> writeToDirectory(List<? extends UniqueIdentifiable> objects,
+  private Map<ObjectId, Integer> writeToDirectory(Iterator<? extends UniqueIdentifiable> objects,
                                                   String outputSubDirName,
                                                   String prefix) throws IOException {
     File outputDir = new File(_outputDir, outputSubDirName);
@@ -247,7 +300,9 @@ import com.opengamma.util.fudgemsg.OpenGammaFudgeContext;
     }
     s_logger.info("Writing to {}", outputDir.getAbsolutePath());
     Map<ObjectId, Integer> ids = Maps.newHashMap();
-    for (UniqueIdentifiable object : objects) {
+    int count = 0;
+    while (objects.hasNext()) {
+      UniqueIdentifiable object = objects.next();
       ObjectId objectId = object.getUniqueId().getObjectId();
       Integer previousId = _idMappings.getId(objectId);
       int id;
@@ -259,8 +314,9 @@ import com.opengamma.util.fudgemsg.OpenGammaFudgeContext;
       }
       String fileName = prefix + id + ".xml";
       writeToFudge(outputDir, object, fileName);
+      count++;
     }
-    s_logger.info("Wrote {} objects to {}", objects.size(), outputDir.getAbsolutePath());
+    s_logger.info("Wrote {} objects to {}", count, outputDir.getAbsolutePath());
     return ids;
   }
 
@@ -284,4 +340,74 @@ import com.opengamma.util.fudgemsg.OpenGammaFudgeContext;
       fudgeMsgWriter.flush();
     }
   }
+  
+  private class SecurityTransformer implements Function<SecurityDocument, ManageableSecurity> {
+    @Override
+    public ManageableSecurity apply(SecurityDocument input) {
+      return input.getSecurity();
+    }
+  }
+  
+  private class PositionTransformer implements Function<PositionDocument, ManageablePosition> {
+    @Override
+    public ManageablePosition apply(PositionDocument input) {
+      return input.getPosition();
+    }
+  }
+  
+  private class PortfolioTransformer implements Function<PortfolioDocument, ManageablePortfolio> {
+    @Override
+    public ManageablePortfolio apply(PortfolioDocument input) {
+      return input.getPortfolio();
+    }
+  }
+  
+  private class ConfigTransformer implements Function<ConfigDocument, ConfigItem<?>> {
+
+    @Override
+    public ConfigItem<?> apply(ConfigDocument input) {
+      return input.getConfig();
+    }
+  }
+  
+  private class TimeSeriesTransformer implements Function<HistoricalTimeSeriesInfoDocument, TimeSeriesWithInfo> {
+    
+    @Override
+    public TimeSeriesWithInfo apply(HistoricalTimeSeriesInfoDocument infoDoc) {
+      ManageableHistoricalTimeSeriesInfo info = infoDoc.getInfo();
+      ManageableHistoricalTimeSeries timeSeries =
+          _timeSeriesMaster.getTimeSeries(info.getTimeSeriesObjectId(), VersionCorrection.LATEST);
+      TimeSeriesWithInfo timeSeriesWithInfo = new TimeSeriesWithInfo(info, timeSeries);
+      return timeSeriesWithInfo;
+    }
+  }
+  
+  private class HolidayTransformer implements Function<HolidayDocument, ManageableHoliday> {
+    @Override
+    public ManageableHoliday apply(HolidayDocument input) {
+      return input.getHoliday();
+    }
+  }
+  
+  private class ExchangeTransformer implements Function<ExchangeDocument, ManageableExchange> {
+    @Override
+    public ManageableExchange apply(ExchangeDocument input) {
+      return input.getExchange();
+    }
+  }
+  
+  private class SnapshotTransformer implements Function<MarketDataSnapshotDocument, ManageableMarketDataSnapshot> {
+    @Override
+    public ManageableMarketDataSnapshot apply(MarketDataSnapshotDocument input) {
+      return input.getSnapshot();
+    }
+  }
+  
+  private class OrganizationTransformer implements Function<OrganizationDocument, ManageableOrganization> {
+    @Override
+    public ManageableOrganization apply(OrganizationDocument input) {
+      return input.getOrganization();
+    }
+  }
+  
 }
