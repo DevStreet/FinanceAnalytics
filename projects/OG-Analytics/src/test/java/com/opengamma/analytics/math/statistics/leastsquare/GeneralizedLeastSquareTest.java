OpenGamma/OG-Platform

[MAT-310] modify tests with rank deficient data to match OG-Maths.

projects/OG-Analytics/src/test/java/com/opengamma/analytics/math/statistics/leastsquare/GeneralizedLeastSquareTest.java
@@ -30,6 +30,7 @@ import com.opengamma.analytics.math.interpolation.PSplineFitter;
 import com.opengamma.analytics.math.interpolation.data.Interpolator1DDataBundle;
 import com.opengamma.analytics.math.matrix.DoubleMatrix1D;
 import com.opengamma.analytics.math.statistics.distribution.NormalDistribution;
+import com.opengamma.longdog.helpers.FuzzyEquals;
 import com.opengamma.util.test.TestGroup;
 
 /**
@@ -200,6 +201,9 @@ public class GeneralizedLeastSquareTest {
 
   }
 
+  // NOTE: OG-Maths devs - These tests are super sensitive as the matrices involved are rank deficient.
+  // The test results are adjusted for the purposes of regression testing to LAPACK 3.4.2.
+  
   @Test
   public void testBSplineFit() {
     final GeneralizedLeastSquare gls = new GeneralizedLeastSquare();
@@ -207,7 +211,7 @@ public class GeneralizedLeastSquareTest {
     final LeastSquareResults results = gls.solve(X, Y, SIGMA, BASIS_FUNCTIONS);
     final Function1D<Double, Double> spline = new BasisFunctionAggregation<>(BASIS_FUNCTIONS, results.getFitParameters().getData());
     assertEquals(0.0, results.getChiSq(), 1e-12);
-    assertEquals(-0.023605293, spline.evaluate(0.5), 1e-8);
+    assertEquals(-26.297333134152204, spline.evaluate(0.5), 1e-8);
 
     if (PRINT) {
       System.out.println("Chi^2:\t" + results.getChiSq());
@@ -229,8 +233,8 @@ public class GeneralizedLeastSquareTest {
 
     final LeastSquareResults results = gls.solve(X_COS_EXP, Y_COS_EXP, SIGMA_COS_EXP, BASIS_FUNCTIONS_2D);
     final Function1D<double[], Double> spline = new BasisFunctionAggregation<>(BASIS_FUNCTIONS_2D, results.getFitParameters().getData());
-    assertEquals(0.0, results.getChiSq(), 1e-25);
-    assertEquals(0.05161579, spline.evaluate(new double[] {4, 3 }), 1e-8);
+    assertTrue(FuzzyEquals.SingleValueFuzzyEquals(0.0, results.getChiSq()));
+    assertEquals(-64958.51700605097, spline.evaluate(new double[] {4, 3 }), 1e-8);
 
     /*
      * Print out function for debugging
@@ -364,7 +368,7 @@ public class GeneralizedLeastSquareTest {
 
     assertEquals(0.0, results.getChiSq(), 1e-9);
     final Function1D<double[], Double> spline = results.getFunction();
-    assertEquals(0.462288104, spline.evaluate(new double[] {4, 3 }), 1e-8);
+    assertEquals(0.5333876461794294, spline.evaluate(new double[] {4, 3 }), 1e-8);
 
     /*
      * Print out function for debugging

DevStreet/FinanceAnalytics

projects/OG-Analytics/src/test/java/com/opengamma/analytics/math/statistics/leastsquare/GeneralizedLeastSquareTest.java
@@ -30,6 +30,7 @@ import com.opengamma.analytics.math.interpolation.PSplineFitter;
 import com.opengamma.analytics.math.interpolation.data.Interpolator1DDataBundle;
 import com.opengamma.analytics.math.matrix.DoubleMatrix1D;
 import com.opengamma.analytics.math.statistics.distribution.NormalDistribution;
+import com.opengamma.maths.helpers.FuzzyEquals;
 import com.opengamma.util.test.TestGroup;
 
 /**
@@ -200,6 +201,9 @@ public class GeneralizedLeastSquareTest {
 
   }
 
+  // NOTE: OG-Maths devs - These tests are super sensitive as the matrices involved are rank deficient.
+  // The test results are adjusted for the purposes of regression testing to LAPACK 3.4.2.
+  
   @Test
   public void testBSplineFit() {
     final GeneralizedLeastSquare gls = new GeneralizedLeastSquare();
@@ -207,7 +211,7 @@ public class GeneralizedLeastSquareTest {
     final LeastSquareResults results = gls.solve(X, Y, SIGMA, BASIS_FUNCTIONS);
     final Function1D<Double, Double> spline = new BasisFunctionAggregation<>(BASIS_FUNCTIONS, results.getFitParameters().getData());
     assertEquals(0.0, results.getChiSq(), 1e-12);
-    assertEquals(-0.023605293, spline.evaluate(0.5), 1e-8);
+    assertEquals(-26.297333134152204, spline.evaluate(0.5), 1e-8);
 
     if (PRINT) {
       System.out.println("Chi^2:\t" + results.getChiSq());
@@ -229,8 +233,9 @@ public class GeneralizedLeastSquareTest {
 
     final LeastSquareResults results = gls.solve(X_COS_EXP, Y_COS_EXP, SIGMA_COS_EXP, BASIS_FUNCTIONS_2D);
     final Function1D<double[], Double> spline = new BasisFunctionAggregation<>(BASIS_FUNCTIONS_2D, results.getFitParameters().getData());
-    assertEquals(0.0, results.getChiSq(), 1e-16);
-    assertEquals(0.05161579, spline.evaluate(new double[] {4, 3 }), 1e-8);
+    assertTrue(FuzzyEquals.SingleValueFuzzyEquals(0.0, results.getChiSq()));
+    assertEquals(-64958.51700605097, spline.evaluate(new double[] {4, 3 }), 1e-8);
+
 
     /*
      * Print out function for debugging
@@ -364,7 +369,7 @@ public class GeneralizedLeastSquareTest {
 
     assertEquals(0.0, results.getChiSq(), 1e-9);
     final Function1D<double[], Double> spline = results.getFunction();
-    assertEquals(0.4635978895963084, spline.evaluate(new double[] {4, 3 }), 1e-8);
+    assertEquals(0.5333876461794294, spline.evaluate(new double[] {4, 3 }), 1e-8);
 
     /*
      * Print out function for debugging