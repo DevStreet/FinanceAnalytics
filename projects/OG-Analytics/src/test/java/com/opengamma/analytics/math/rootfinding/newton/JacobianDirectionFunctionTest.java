efcc25aa1a97e36ab8d552fab56a22621b4b81a0
Stuart Archibald <stuart@opengamma.com>
2014-10-01 12:31:49 +0100

    [MAT-477] fix up JacobianDirectionFunctionTest

--- projects/OG-Analytics/src/test/java/com/opengamma/analytics/math/rootfinding/newton/JacobianDirectionFunctionTest.java
+++ projects/OG-Analytics/src/test/java/com/opengamma/analytics/math/rootfinding/newton/JacobianDirectionFunctionTest.java
@@ -9,7 +9,6 @@ import static org.testng.AssertJUnit.assertEquals;
 
 import org.testng.annotations.Test;
 
-import com.opengamma.analytics.math.linearalgebra.SVDecompositionColt;
 import com.opengamma.analytics.math.matrix.DoubleMatrix1D;
 import com.opengamma.analytics.math.matrix.DoubleMatrix2D;
 import com.opengamma.util.test.TestGroup;
@@ -19,18 +18,12 @@ import com.opengamma.util.test.TestGroup;
  */
 @Test(groups = TestGroup.UNIT)
 public class JacobianDirectionFunctionTest {
-  private static final SVDecompositionColt SV = new SVDecompositionColt();
-  private static final JacobianDirectionFunction F = new JacobianDirectionFunction(SV);
+  private static final JacobianDirectionFunction F = new JacobianDirectionFunction();
   private static final double X0 = 2.4;
   private static final double X1 = 7.6;
   private static final double X2 = 4.5;
-  private static final DoubleMatrix2D M = new DoubleMatrix2D(new double[][] {new double[] {X0, 0, 0}, new double[] {0, X1, 0}, new double[] {0, 0, X2}});
-  private static final DoubleMatrix1D Y = new DoubleMatrix1D(new double[] {1, 1, 1});
-
-  @Test(expectedExceptions = IllegalArgumentException.class)
-  public void testNull() {
-    new JacobianDirectionFunction(null);
-  }
+  private static final DoubleMatrix2D M = new DoubleMatrix2D(new double[][] { new double[] { X0, 0, 0 }, new double[] { 0, X1, 0 }, new double[] { 0, 0, X2 } });
+  private static final DoubleMatrix1D Y = new DoubleMatrix1D(new double[] { 1, 1, 1 });
 
   @Test(expectedExceptions = IllegalArgumentException.class)
   public void testNullEstimate() {
